16305a379097c457c99cb78580f51517fe0c7b5f
Michael Moyle <michaeljmoyle@gmail.com>
2020-09-15 15:13:47 +0200

    refactor: change libary path

--- plugins/modules/elb_security_group.py
+++ plugins/modules/elb_security_group.py
@@ -52,8 +52,8 @@ response:
     type: int
 '''
 
-from ansible.module_utils.aws.core import AnsibleAWSModule
-from ansible.module_utils.ec2 import AWSRetry
+from ansible_collections.amazon.aws.plugins.module_utils.core import AnsibleAWSModule
+from ansible_collections.amazon.aws.plugins.module_utils.ec2 import AWSRetry
 
 try:
     from botocore.exceptions import ClientError
