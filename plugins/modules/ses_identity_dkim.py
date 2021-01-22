6dd6b72278f10f5231999b3ee77e98efef775875
Stefan Horning <stefan@hornings.de>
2021-01-22 13:20:18 +0100

    Fix module utils namespace

--- plugins/modules/ses_identity_dkim.py
+++ plugins/modules/ses_identity_dkim.py
@@ -83,7 +83,7 @@ dkim_attributes:
             sample: 'Pending'
 '''
 
-from ansible_collections.amazon.aws.plugins.module_utils.aws.core import AnsibleAWSModule
+from ansible_collections.amazon.aws.plugins.module_utils.core import AnsibleAWSModule
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import camel_dict_to_snake_dict, AWSRetry
 
 try:
