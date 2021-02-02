dec9b0522ae4921b0bb443cd654f62697df8aaca
Milan Zink <mzink@redhat.com>
2021-02-02 14:27:27 +0100

    Documentation update
    - fix wording and typos in DOCUMENTATION
    - complete rewrite of RETURN

--- plugins/modules/aws_s3_bucket_info.py
+++ plugins/modules/aws_s3_bucket_info.py
@@ -13,12 +13,12 @@ DOCUMENTATION = '''
 module: aws_s3_bucket_info
 version_added: 1.0.0
 author: "Gerben Geijteman (@hyperized)"
-short_description: Lists S3 buckets in AWS
+short_description: lists S3 buckets in AWS
 requirements:
   - boto3 >= 1.4.4
   - python >= 2.6
 description:
-    - Lists S3 buckets in AWS
+    - Lists S3 buckets and details about those buckets.
     - This module was called C(aws_s3_bucket_facts) before Ansible 2.9, returning C(ansible_facts).
       Note that the M(community.aws.aws_s3_bucket_info) module no longer returns C(ansible_facts)!
 options:
@@ -30,95 +30,89 @@ options:
     version_added: 1.4.0
   name_filter:
     description:
-      - Get information only about buckets name containing a string.
+      - Limits buckets to only buckets who's name contain the string in I(name_filter).
     type: str
     default: ""
     version_added: 1.4.0
   bucket_facts:
     description:
       - Retrieve requested S3 bucket detailed information
-      - Each bucket_X option executes one API call, hence many options=true will case slower module execution
-      - You can limit buckets by using I(name) or I(name_filter) option
+      - Each bucket_X option executes one API call, hence many options being set to C(true) will cause slower module execution.
+      - You can limit buckets by using the I(name) or I(name_filter) option.
     suboptions:
       bucket_accelerate_configuration:
-        description: Retrive S3 accelerate configuration
+        description: Retrive S3 accelerate configuration.
         type: bool
         default: False
       bucket_location:
-        description: Retrive S3 bucket location
+        description: Retrive S3 bucket location.
         type: bool
         default: False
       bucket_replication:
-        description: Retrive S3 bucket replication
+        description: Retrive S3 bucket replication.
         type: bool
         default: False
       bucket_acl:
-        description: Retrive S3 bucket ACLs
+        description: Retrive S3 bucket ACLs.
         type: bool
         default: False
       bucket_logging:
-        description: Retrive S3 bucket logging
+        description: Retrive S3 bucket logging.
         type: bool
         default: False
       bucket_request_payment:
-        description: Retrive S3 bucket request payment
+        description: Retrive S3 bucket request payment.
         type: bool
         default: False
       bucket_tagging:
-        description: Retrive S3 bucket tagging
+        description: Retrive S3 bucket tagging.
         type: bool
         default: False
       bucket_cors:
-        description: Retrive S3 bucket CORS configuration
+        description: Retrive S3 bucket CORS configuration.
         type: bool
         default: False
       bucket_notification_configuration:
-        description: Retrive S3 bucket notification configuration
+        description: Retrive S3 bucket notification configuration.
         type: bool
         default: False
       bucket_encryption:
-        description: Retrive S3 bucket encryption
+        description: Retrive S3 bucket encryption.
         type: bool
         default: False
       bucket_ownership_controls:
-        description: Retrive S3 ownership controls
+        description: Retrive S3 ownership controls.
         type: bool
         default: False
       bucket_website:
-        description: Retrive S3 bucket website
+        description: Retrive S3 bucket website.
         type: bool
         default: False
       bucket_policy:
-        description: Retrive S3 bucket policy
+        description: Retrive S3 bucket policy.
         type: bool
         default: False
       bucket_policy_status:
-        description: Retrive S3 bucket policy status
+        description: Retrive S3 bucket policy status.
         type: bool
         default: False
       bucket_lifecycle_configuration:
-        description: Retrive S3 bucket lifecycle configuration
+        description: Retrive S3 bucket lifecycle configuration.
         type: bool
         default: False
       public_access_block:
-        description: Retrive S3 bucket public access block
+        description: Retrive S3 bucket public access block.
         type: bool
         default: False
     type: dict
     version_added: 1.4.0
   transform_location:
     description:
-      - S3 bucket location for default us-east-1 is normally reported as 'null'
-      - setting this option to 'true' will return 'us-east-1' instead
-      - affects only queries with I(bucket_facts) > I(bucket_location) = true
+      - S3 bucket location for default us-east-1 is normally reported as C(null).
+      - Setting this option to C(true) will return C(us-east-1) instead.
+      - Affects only queries with I(bucket_facts=true) and I(bucket_location=true).
     type: bool
-    default: False
-    version_added: 1.4.0
-
-extends_documentation_fragment:
-- amazon.aws.aws
-- amazon.aws.ec2
-
+    default: FalseBucket public access block configuration
 '''
 
 EXAMPLES = '''
@@ -162,17 +156,243 @@ EXAMPLES = '''
 '''
 
 RETURN = '''
-buckets:
+bucket_list:
   description: "List of buckets"
   returned: always
-  sample:
-    - creation_date: '2017-07-06 15:05:12 +00:00'
-      name: my_bucket
-      # bucket facts if requested
-      bucket_location: dictionary data
-      bucket_cors: dictionary data
-      # ...etc
-  type: list
+  type: complex
+  contains:
+    name:
+      description: Bucket name.
+      returned: always
+      type: str
+      sample: a-testing-bucket-name
+    creation_date:
+      description: Bucket creation date timestamp.
+      returned: always
+      type: str
+      sample: "2021-01-21T12:44:10+00:00"
+    public_access_block:
+      description: Bucket public access block configuration.
+      returned: when I(bucket_facts=true) and I(public_access_block=true)
+      type: complex
+      contains:
+        PublicAccessBlockConfiguration:
+          description: PublicAccessBlockConfiguration data.
+          returned: when PublicAccessBlockConfiguration is defined for the bucket
+          type: complex
+          contains:
+            BlockPublicAcls:
+              description: BlockPublicAcls setting value.
+              type: bool
+              sample: true
+            BlockPublicPolicy:
+              description: BlockPublicPolicy setting value.
+              type: bool
+              sample: true
+            IgnorePublicAcls:
+              description: IgnorePublicAcls setting value.
+              type: bool
+              sample: true
+            RestrictPublicBuckets:
+              description: RestrictPublicBuckets setting value.
+              type: bool
+              sample: true
+    bucket_name_filter:
+      description: String used to limit buckets. See I(name_filter).
+      returned: when I(name_filter) is defined
+      type: str
+      sample: filter-by-this-string
+    bucket_acl:
+      description: Bucket ACL configuration.
+      returned: when I(bucket_facts=true) and I(bucket_acl=true)
+      type: complex
+      contains:
+        Grants:
+          description: List of ACL grants.
+          type: list
+          sample: []
+        Owner:
+          description: Bucket owner information.
+          type: complex
+          contains:
+            DisplayName:
+              description: Bucket owner user display name.
+              returned: str
+              sample: username
+            ID:
+              description: Bucket owner user ID.
+              returned: str
+              sample: 123894e509349etc
+    bucket_cors:
+      description: Bucket CORS configuration.
+      returned: when I(bucket_facts=true) and I(bucket_cors=true)
+      type: complex
+      contains:
+        CORSRules:
+          description: Bucket CORS configuration.
+          returned: when CORS rules are defined for the bucket
+          type: list
+          sample: []
+    bucket_encryption:
+      description: Bucket encryption configuration.
+      returned: when I(bucket_facts=true) and I(bucket_encryption=true)
+      type: complex
+      contains:
+        ServerSideEncryptionConfiguration:
+          description: ServerSideEncryptionConfiguration configuration.
+          returned: when encryption is enabled on the bucket
+          type: complex
+          contains:
+            Rules:
+              description: List of applied encryptio rules.
+              returned: when encryption is enabled on the bucket
+              type: list
+              sample: { "ApplyServerSideEncryptionByDefault": { "SSEAlgorithm": "AES256" }, "BucketKeyEnabled": False }
+      bucket_lifecycle_configuration:
+        description: Bucket lifecycle configuration settings.
+        returned: when I(bucket_facts=true) and I(bucket_lifecycle_configuration=true)
+        type: complex
+        contains:
+          Rules:
+            description: List of lifecycle management rules.
+            returned: when lifecycle configuration is present
+            type: list
+            sample: [{ "Status": "Enabled", "ID": "example-rule" }]
+      bucket_location:
+        description: Bucket location.
+        returned: when I(bucket_facts=true) and I(bucket_location=true)
+        type: complex
+        contains:
+          LocationConstraint:
+            description: AWS region.
+            returned: always
+            type: str
+            sample: us-east-2
+      bucket_logging:
+        description: Server access logging configuration.
+        returned: when I(bucket_facts=true) and I(bucket_logging=true)
+        type: complex
+        contains:
+          LoggingEnabled:
+            description: Server access logging configuration.
+            returned: when server access logging is defined for the bucket
+            type: complex
+            contains:
+              TargetBucket:
+                description: Target bucket name.
+                returned: always
+                type: str
+                sample: logging-bucket-name
+              TargetPrefix:
+                description: Prefix in target bucket.
+                returned: always
+                type: str
+                sample: ""
+      bucket_notification_configuration:
+        description: Bucket notification settings.
+        returned: when I(bucket_facts=true) and I(bucket_notification_configuration=true)
+        type: complex
+        contains:
+          TopicConfigurations:
+            description: List of notification events configurations.
+            returned: when at least one notification is configured
+            type: list
+            sample: []
+      bucket_ownership_controls:
+        description: Preffered object ownership settings.
+        returned: when I(bucket_facts=true) and I(bucket_ownership_controls=true)
+        type: complex
+        contains:
+          OwnershipControls:
+            description: Object ownership settings.
+            returned: when ownership controls are defined for the bucket
+            type: complex
+            contains:
+              Rules:
+                description: List of ownership rules.
+                returned: when ownership rule is defined
+                type: list
+                sample: [{ "ObjectOwnership:": "ObjectWriter" }]
+      bucket_policy:
+        description: Bucket policy contents.
+        returned: when I(bucket_facts=true) and I(bucket_policy=true)
+        type: str
+        sample: '{"Version":"2012-10-17","Statement":[{"Sid":"AddCannedAcl","Effect":"Allow",..}}]}'
+      bucket_policy_status:
+        description: Status of bucket policy.
+        returned: when I(bucket_facts=true) and I(bucket_policy_status=true)
+        type: complex
+        contains:
+          PolicyStatus:
+            description: Status of bucket policy.
+            returned: when bucket policy is present
+            type: complex
+            contains:
+              IsPublic:
+                description: Report bucket policy public status.
+                returned: when bucket policy is present
+                type: bool
+                sample: True
+      bucket_replication:
+        description: Replication configuration settings.
+        returned: when I(bucket_facts=true) and I(bucket_replication=true)
+        type: complex
+        contains:
+          Role:
+            description: IAM role used for replication.
+            returned: when replication rule is defined
+            type: str
+            sample: "arn:aws:iam::123:role/example-role"
+          Rules:
+            description: List of replication rules.
+            returned: when replication rule is defined
+            type: list
+            sample: [{ "ID": "rule-1", "Filter": "{}" }]
+      bucket_request_payment:
+        description: Requester pays setting.
+        returned: when I(bucket_facts=true) and I(bucket_request_payment=true)
+        type: complex
+        contains:
+          Payer:
+            description: Current payer.
+            returned: always
+            type: str
+            sample: BucketOwner
+      bucket_tagging:
+        description: Bucket tags.
+        returned: when I(bucket_facts=true) and I(bucket_tagging=true)
+        type: dict
+        sample: { "Tag1": "Value1", "Tag2": "Value2" }
+      bucket_website:
+        description: Static website hosting.
+        returned: when I(bucket_facts=true) and I(bucket_website=true)
+        type: complex
+        contains:
+          ErrorDocument:
+            description: Object serving as HTTP error page.
+            returned: when static website hosting is enabled
+            type: dict
+            sample: { "Key": "error.html" }
+          IndexDocument:
+            description: Object serving as HTTP index page.
+            returned: when static website hosting is enabled
+            type: dict
+            sample: { "Suffix": "error.html" }
+          RedirectAllRequestsTo:
+            description: Website redict settings.
+            returned: when redirect requests is configured
+            type: complex
+            contains:
+              HostName:
+                description: Hostname to redirect.
+                returned: always
+                type: str
+                sample: www.example.com
+              Protocol:
+                description: Protocol used for redirect.
+                returned: always
+                type: str
+                sample: https
 '''
 
 try:
