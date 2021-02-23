8d2f9512db65f42f79a777d821c2e6b4821c4c0d
Stefan Horning <stefan@hornings.de>
2021-02-23 13:50:37 +0100

    Fix indentation of added method

--- plugins/modules/ec2_launch_template.py
+++ plugins/modules/ec2_launch_template.py
@@ -482,13 +482,13 @@ def delete_template(module):
 
 
 def remove_none(obj):
-  if isinstance(obj, (list, tuple, set)):
-    return type(obj)(remove_none(x) for x in obj if x is not None)
-  elif isinstance(obj, dict):
-    return type(obj)((remove_none(k), remove_none(v))
-      for k, v in obj.items() if k is not None and v is not None)
-  else:
-    return obj
+    if isinstance(obj, (list, tuple, set)):
+        return type(obj)(remove_none(x) for x in obj if x is not None)
+    elif isinstance(obj, dict):
+        return type(obj)((remove_none(k), remove_none(v))
+            for k, v in obj.items() if k is not None and v is not None)
+    else:
+        return obj
 
 
 def create_or_update(module, template_options):
