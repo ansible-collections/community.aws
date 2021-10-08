54cf169e483b5e6e6e033a13477eeff1d13f5af3
mark-woolley <mw@marknet15.com>
2021-10-08 09:34:08 +0100

    Revert "Squashed commit of the following:"
    This reverts commit 8d9aacc5c9655dea2483c08dc37948486fe94cbe.

--- plugins/modules/route53.py
+++ plugins/modules/route53.py
@@ -606,7 +606,6 @@ def main():
         'TTL': ttl_in,
         'ResourceRecords': [dict(Value=value) for value in value_in],
         'HealthCheckId': health_check_in,
-        'SetIdentifier': identifier_in,
     })
 
     if alias_in:
