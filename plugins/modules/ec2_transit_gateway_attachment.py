b8ba49d1e61897121e84ba068b13833e47e57b2e
Chris Forkner <chris.forkner@alchemer.com>
2021-11-25 17:54:55 -0700

    Some logic for comparisons and general cleanup

--- plugins/modules/ec2_transit_gateway_attachment.py
+++ plugins/modules/ec2_transit_gateway_attachment.py
@@ -3,6 +3,7 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
+from os import remove
 __metaclass__ = type
 
 
@@ -71,104 +72,25 @@ class AnsibleEc2TgwAttachment(object):
     def __init__(self, module, results):
         self._module = module
         self._results = results
-        self._connection = self._moduel.client('ec2')
+        self._connection = self._module.client('ec2')
         self._check_mode = self._module.check_mode
 
 
     def process(self):
-        """ Process the request based on state parameter .
-          state = present will search for an existing tgw based and return the object data.
-            if no object is found it will be created
-
-          state = absent will attempt to remove the tgw however will fail if it still has
-            attachments or associations
-          """
         description = self._module.params.get('description')
         state = self._module.params.get('state', 'present')
-        tgw_id = self._module.params.get('transit_gateway_id')
+        attach_id = self._module.params.get('attachment_id')
+        if self._module.params.get('vpc_id'):
+            self._attachment_type = "vpc"
+        elif self._module.params.get('peer_transit_gateway'):
+            self._attachment_type = "peer"
 
-        if state == 'present':
-            self.ensure_tgw_present(tgw_id, description)
-        elif state == 'absent':
-            self.ensure_tgw_absent(tgw_id, description)
+        if state == "present":
+            if self._attachment_type == "vpc":
+                vpc_result = self.vpc_ensure_attachment()
+                self._results['transit_gateway_attachment'] = vpc_result
 
-    def wait_for_status(self, wait_timeout, tgw_id, status, skip_deleted=True):
-        """
-        Wait for the Transit Gateway to reach  the specified status
-        :param wait_timeout: Number of seconds to wait, until this timeout is reached.
-        :param tgw_id: The Amazon nat id.
-        :param status: The status to wait for.
-                examples. status=available, status=deleted
-        :param skip_deleted: ignore deleted transit gateways
-        :return dict: transit gateway object
-        """
-        polling_increment_secs = 5
-        wait_timeout = time() + wait_timeout
-        status_achieved = False
-        transit_gateway = dict()
-
-        while wait_timeout > time():
-            try:
-                transit_gateway = self.get_matching_tgw(tgw_id=tgw_id, skip_deleted=skip_deleted)
-
-                if transit_gateway:
-                    if self._check_mode:
-                        transit_gateway['state'] = status
-
-                    if transit_gateway.get('state') == status:
-                        status_achieved = True
-                        break
-
-                    elif transit_gateway.get('state') == 'failed':
-                        break
-
-                else:
-                    sleep(polling_increment_secs)
-
-            except ClientError as e:
-                self._module.fail_json_aws(e)
-
-        if not status_achieved:
-            self._module.fail_json(
-                msg="Wait time out reached, while waiting for results")
-
-        return transit_gateway
-
-    def get_matching_tgw(self, tgw_id, description=None, skip_deleted=True):
-        """ search for  an existing tgw by either tgw_id or description
-        :param tgw_id:  The AWS id of the transit gateway
-        :param description:  The description of the transit gateway.
-        :param skip_deleted: ignore deleted transit gateways
-        :return dict: transit gateway object
-        """
-        filters = []
-        if tgw_id:
-            filters = ansible_dict_to_boto3_filter_list({'transit-gateway-id': tgw_id})
-
-        try:
-            response = AWSRetry.exponential_backoff()(self._connection.describe_transit_gateways)(Filters=filters)
-        except (ClientError, BotoCoreError) as e:
-            self._module.fail_json_aws(e)
-
-        tgw = None
-        tgws = []
-
-        if len(response.get('TransitGateways', [])) == 1 and tgw_id:
-            if (response['TransitGateways'][0]['State'] != 'deleted') or not skip_deleted:
-                tgws.extend(response['TransitGateways'])
-
-        for gateway in response.get('TransitGateways', []):
-            if description == gateway['Description'] and gateway['State'] != 'deleted':
-                tgws.append(gateway)
-
-        if len(tgws) > 1:
-            self._module.fail_json(
-                msg='EC2 returned more than one transit Gateway for description {0}, aborting'.format(description))
-        elif tgws:
-            tgw = camel_dict_to_snake_dict(tgws[0], ignore_list=['Tags'])
-            tgw['tags'] = boto3_tag_list_to_ansible_dict(tgws[0]['Tags'])
-
-        return tgw
+                return self._results
 
     @staticmethod
     def enable_option_flag(flag):
@@ -178,176 +100,110 @@ class AnsibleEc2TgwAttachment(object):
             return enabled
         return disabled
 
-    def create_tgw(self, description):
-        """
-        Create a transit gateway and optionally wait for status to become available.
+    @staticmethod
+    def compare_tags(current_tags, desired_tags):
+        tags_to_add = {}
+        tags_to_remove = {}
 
-        :param description: The description of the transit gateway.
-        :return dict: transit gateway object
-        """
-        options = dict()
-        wait = self._module.params.get('wait')
-        wait_timeout = self._module.params.get('wait_timeout')
+        for k,v in desired_tags.items():
+            if k not in current_tags.keys():
+                tags_to_add[k] = v
+                continue
+            elif current_tags[k] != v:
+                tags_to_add[k] = v
+                continue
 
-        if self._module.params.get('asn'):
-            options['AmazonSideAsn'] = self._module.params.get('asn')
+        for k,v in current_tags.items():
+            if k not in desired_tags.keys():
+                tags_to_remove[k] = v
 
-        options['AutoAcceptSharedAttachments'] = self.enable_option_flag(self._module.params.get('auto_attach'))
-        options['DefaultRouteTableAssociation'] = self.enable_option_flag(self._module.params.get('auto_associate'))
-        options['DefaultRouteTablePropagation'] = self.enable_option_flag(self._module.params.get('auto_propagate'))
-        options['VpnEcmpSupport'] = self.enable_option_flag(self._module.params.get('vpn_ecmp_support'))
-        options['DnsSupport'] = self.enable_option_flag(self._module.params.get('dns_support'))
 
-        try:
-            response = self._connection.create_transit_gateway(Description=description, Options=options)
-        except (ClientError, BotoCoreError) as e:
-            self._module.fail_json_aws(e)
+        return tags_to_add, tags_to_remove
 
-        tgw_id = response['TransitGateway']['TransitGatewayId']
+    def vpc_check_existing(self, vpc_id, tgw_id):
+        filter_dict = {
+            "vpc-id": vpc_id,
+            "transit-gateway-id": tgw_id
+        }
+        filters = ansible_dict_to_boto3_filter_list(filter_dict)
+        vpc_response = self._connection.describe_transit_gateway_vpc_attachments(Filters=filters)
+        
 
-        if wait:
-            result = self.wait_for_status(wait_timeout=wait_timeout, tgw_id=tgw_id, status="available")
-        else:
-            result = self.get_matching_tgw(tgw_id=tgw_id)
+        return vpc_response.get('TransitGatewayVpcAttachments', [])
 
-        self._results['msg'] = (' Transit gateway {0} created'.format(result['transit_gateway_id']))
+    def tgw_update_tags(self, attachment_id, current_tags, desired_tags):
+        return
 
-        return result
+    @staticmethod
+    def vpc_compare_subnets(current_subnets, desired_subnets):
+        add_subnets = set(desired_subnets).difference(current_subnets)
+        remove_subnets = set(current_subnets).difference(desired_subnets)
 
-    def delete_tgw(self, tgw_id):
-        """
-        De;lete the transit gateway and optionally wait for status to become deleted
+        return add_subnets, remove_subnets
 
-        :param tgw_id: The id of the transit gateway
-        :return dict: transit gateway object
-        """
-        wait = self._module.params.get('wait')
-        wait_timeout = self._module.params.get('wait_timeout')
+    @AWSRetry.exponential_backoff()
+    def vpc_compare_and_update(self, tgw_attachment):
+        changed = False
+        
+        tgw_options = {
+                "ApplianceModeSupport": self.enable_option_flag(self._module.params.get('vpc_dns_support')),
+                "DnsSupport": self.enable_option_flag(self._module.params.get('vpc_ipv6_support')),
+                "Ipv6Support": self.enable_option_flag(self._module.params.get('vpc_appliance_mode_support'))
+        }
 
-        try:
-            response = self._connection.delete_transit_gateway(TransitGatewayId=tgw_id)
-        except (ClientError, BotoCoreError) as e:
-            self._module.fail_json_aws(e)
+        subnets_to_add, subnets_to_remove = self.vpc_compare_subnets(
+            tgw_attachment['SubnetIds'],
+            self._module.params.get('subnet_ids')
+        )
 
-        if wait:
-            result = self.wait_for_status(wait_timeout=wait_timeout, tgw_id=tgw_id, status="deleted", skip_deleted=False)
-        else:
-            result = self.get_matching_tgw(tgw_id=tgw_id, skip_deleted=False)
+        tags_to_add, tags_to_remove = self.compare_tags(
+                boto3_tag_list_to_ansible_dict(tgw_attachment.get('Tags', None)),
+                self._module.params.get('tags')
+            )
 
-        self._results['msg'] = (' Transit gateway {0} deleted'.format(tgw_id))
+        if tgw_options != tgw_attachment['Options']:
+            changed = True
+        elif len(tags_to_add) > 0 or len(tags_to_remove) > 0:
+            changed = True
+        elif len(subnets_to_add) > 0 or len(subnets_to_remove) > 0:
+            changed = True
 
-        return result
+        
 
-    def ensure_tags(self, tgw_id, tags, purge_tags):
-        """
-        Ensures tags are applied to the transit gateway.  Optionally will remove any
-        existing tags not in the tags argument if purge_tags is set to true
+        # if len(tags_to_add) > 0:
+            
 
-        :param tgw_id:  The AWS id of the transit gateway
-        :param tags:  list of tags to  apply to the  transit gateway.
-        :param purge_tags:  when true existing tags not in tags parms are removed
-        :return:  true if tags were updated
-        """
-        tags_changed = False
-        filters = ansible_dict_to_boto3_filter_list({'resource-id': tgw_id})
-        try:
-            cur_tags = self._connection.describe_tags(Filters=filters)
-        except (ClientError, BotoCoreError) as e:
-            self._module.fail_json_aws(e, msg="Couldn't describe tags")
+        # if len(tags_to_remove) > 0:
 
-        to_update, to_delete = compare_aws_tags(boto3_tag_list_to_ansible_dict(cur_tags.get('Tags')), tags, purge_tags)
 
-        if to_update:
+
+        if changed:
             try:
                 if not self._check_mode:
-                    AWSRetry.exponential_backoff()(self._connection.create_tags)(
-                        Resources=[tgw_id],
-                        Tags=ansible_dict_to_boto3_tag_list(to_update)
-                    )
-                self._results['changed'] = True
-                tags_changed = True
-            except (ClientError, BotoCoreError) as e:
-                self._module.fail_json_aws(e, msg="Couldn't create tags {0} for resource {1}".format(
-                    ansible_dict_to_boto3_tag_list(to_update), tgw_id))
-
-        if to_delete:
-            try:
-                if not self._check_mode:
-                    tags_list = []
-                    for key in to_delete:
-                        tags_list.append({'Key': key})
-
-                    AWSRetry.exponential_backoff()(self._connection.delete_tags)(
-                        Resources=[tgw_id],
-                        Tags=tags_list
-                    )
-                self._results['changed'] = True
-                tags_changed = True
-            except (ClientError, BotoCoreError) as e:
-                self._module.fail_json_aws(e, msg="Couldn't delete tags {0} for resource {1}".format(
-                    ansible_dict_to_boto3_tag_list(to_delete), tgw_id))
-
-        return tags_changed
-
-    def ensure_tgw_present(self, tgw_id=None, description=None):
-        """
-        Will create a tgw if no match to the tgw_id or description are found
-        Will update the tgw tags if matching one found but tags are not synced
-
-        :param tgw_id:  The AWS id of the transit gateway
-        :param description:  The description of the transit gateway.
-        :return dict: transit gateway object
-        """
-        tgw = self.get_matching_tgw(tgw_id, description)
-
-        if tgw is None:
-            if self._check_mode:
-                self._results['changed'] = True
-                self._results['transit_gateway_id'] = None
-                return self._results
-
-            try:
-                if not description:
-                    self._module.fail_json(msg="Failed to create Transit Gateway: description argument required")
-                tgw = self.create_tgw(description)
+                    # response =self._connection.modify_transit_gateway_vpc_attachment(**update_params)
+                    return
                 self._results['changed'] = True
             except (BotoCoreError, ClientError) as e:
-                self._module.fail_json_aws(e, msg='Unable to create Transit Gateway')
+                self._module.fail_json_aws(e, msg="Trying to update transit gateway attachment configuration")
 
-        if self._module.params.get('tags') != tgw.get('tags'):
-            stringed_tags_dict = dict((to_text(k), to_text(v)) for k, v in self._module.params.get('tags').items())
-            if self.ensure_tags(tgw['transit_gateway_id'], stringed_tags_dict, self._module.params.get('purge_tags')):
-                self._results['changed'] = True
+        
 
-        self._results['transit_gateway'] = self.get_matching_tgw(tgw_id=tgw['transit_gateway_id'])
+        else:
+            return current_tags, desired_tags
 
-        return self._results
 
-    def ensure_tgw_absent(self, tgw_id=None, description=None):
-        """
-        Will delete the tgw if a single tgw is found not yet in deleted status
+    def vpc_ensure_attachment(self):
+        tgw_id = self._module.params.get('transit_gateway_id')
+        vpc_id = self._module.params.get('vpc_id')
 
-        :param tgw_id:  The AWS id of the transit gateway
-        :param description:  The description of the transit gateway.
-        :return doct: transit gateway object
-        """
-        self._results['transit_gateway_id'] = None
-        tgw = self.get_matching_tgw(tgw_id, description)
+        existing = self.vpc_check_existing(vpc_id, tgw_id)
 
-        if tgw is not None:
-            if self._check_mode:
-                self._results['changed'] = True
-                return self._results
-
-            try:
-                tgw = self.delete_tgw(tgw_id=tgw['transit_gateway_id'])
-                self._results['changed'] = True
-                self._results['transit_gateway'] = self.get_matching_tgw(tgw_id=tgw['transit_gateway_id'],
-                                                                         skip_deleted=False)
-            except (BotoCoreError, ClientError) as e:
-                self._module.fail_json_aws(e, msg='Unable to delete Transit Gateway')
-
-        return self._results
+        if len(existing) == 0:
+            return
+        elif len(existing) == 1:
+            return self.vpc_compare_and_update(existing[0])
+        else:
+            return "BORK!"
 
 
 def setup_module_object():
@@ -373,7 +229,8 @@ def setup_module_object():
         state=dict(default='present', choices=['present', 'absent', 'accept', 'reject']),
         
         wait=dict(type='bool', default='yes'),
-        wait_timeout=dict(type='int', default=300)
+        wait_timeout=dict(type='int', default=300),
+        tags=dict(type='dict', default={})
     )
 
     mutually_exclusive = [
@@ -386,7 +243,7 @@ def setup_module_object():
 
     required_together = [
       ['vpc_id','subnet_ids'],
-      ['peer_transit_gateway_id','peer_account_id','peer_region']
+      ['peer_transit_gateway_id','peer_region']
     ]
 
     module = AnsibleAWSModule(
@@ -408,8 +265,8 @@ def main():
         changed=False
     )
 
-    # tgw_manager = AnsibleEc2Tgw(module=module, results=results)
-    # tgw_manager.process()
+    tgw_manager = AnsibleEc2TgwAttachment(module=module, results=results)
+    tgw_manager.process()
 
     module.exit_json(**results)
 
