0c0c72ace7116b5d23ffbd675087dd52a4059f30
Razique <rmahroua@mailbox.org>
2022-03-31 19:39:49 -0700

    Bump to minor fix per unit test feedback

--- plugins/modules/iam_role_info.py
+++ plugins/modules/iam_role_info.py
@@ -9,7 +9,7 @@ __metaclass__ = type
 DOCUMENTATION = '''
 ---
 module: iam_role_info
-version_added: 1.0.1
+version_added: 1.1.0
 short_description: Gather information on IAM roles
 description:
     - Gathers information about IAM roles.
