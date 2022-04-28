0e4f360ff11ffb76597d15e7c5730f83d739d5f0
Mark Chappell <mchappel@redhat.com>
2022-04-28 12:28:57 +0200

    Initial boto3 mixins

--- plugins/module_utils/networkfirewall.py
+++ plugins/module_utils/networkfirewall.py
@@ -99,6 +99,33 @@ class NetworkFirewallWaiterFactory(BaseWaiterFactory):
                     dict(state='success', matcher='error', expected='ResourceNotFoundException'),
                 ]
             ),
+            firewall_active=dict(
+                operation='DescribeFirewall',
+                delay=5, maxAttempts=120,
+                acceptors=[
+                    dict(state='failure', matcher='path', expected='DELETING', argument='FirewallStatus.Status'),
+                    dict(state='retry', matcher='path', expected='PROVISIONING', argument='FirewallStatus.Status'),
+                    dict(state='success', matcher='path', expected='READY', argument='FirewallStatus.Status'),
+                ]
+            ),
+            firewall_updated=dict(
+                operation='DescribeFirewall',
+                delay=5, maxAttempts=120,
+                acceptors=[
+                    dict(state='failure', matcher='path', expected='DELETING', argument='FirewallStatus.Status'),
+                    dict(state='retry', matcher='path', expected='PROVISIONING', argument='FirewallStatus.Status'),
+                    dict(state='retry', matcher='path', expected='PENDING', argument='FirewallStatus.ConfigurationSyncStateSummary'),
+                    dict(state='success', matcher='path', expected='IN_SYNC', argument='FirewallStatus.ConfigurationSyncStateSummary'),
+                ]
+            ),
+            firewall_deleted=dict(
+                operation='DescribeFirewall',
+                delay=5, maxAttempts=120,
+                acceptors=[
+                    dict(state='retry', matcher='path', expected='DELETING', argument='FirewallStatus.Status'),
+                    dict(state='success', matcher='error', expected='ResourceNotFoundException'),
+                ]
+            ),
         )
         data.update(nw_data)
         return data
@@ -246,6 +273,144 @@ class NFPolicyBoto3Mixin(NetworkFirewallBoto3Mixin):
     @Boto3Mixin.aws_error_handler('firewall policy to become active')
     def _wait_policy_active(self, **params):
         waiter = self.nf_waiter_factory.get_waiter('policy_active')
+        waiter.wait(**params)
+
+
+class NFFirewallBoto3Mixin(NetworkFirewallBoto3Mixin):
+    # Paginators can't be (easily) wrapped, so we wrap this method with the
+    # retry - retries the full fetch, but better than simply giving up.
+    @AWSRetry.jittered_backoff()
+    def _paginated_list_firewalls(self, **params):
+        paginator = self.client.get_paginator('list_firewalls')
+        result = paginator.paginate(**params).build_full_result()
+        return result.get('Firewalls', None)
+
+    @Boto3Mixin.aws_error_handler('list all firewalls')
+    def _list_firewalls(self, **params):
+        return self._paginated_list_firewalls(**params)
+
+    @Boto3Mixin.aws_error_handler('describe firewall')
+    def _describe_firewall(self, **params):
+        try:
+            result = self.client.describe_firewall(aws_retry=True, **params)
+        except is_boto3_error_code('ResourceNotFoundException'):
+            return None
+
+        update_token = result.get('UpdateToken', None)
+        if update_token:
+            self._update_token = update_token
+        firewall = result.get('Firewall', None)
+        metadata = result.get('FirewallStatus', None)
+        return dict(Firewall=firewall, FirewallMetadata=metadata)
+
+    @Boto3Mixin.aws_error_handler('create firewall')
+    def _create_firewall(self, **params):
+        result = self.client.create_firewall(aws_retry=True, **params)
+
+        update_token = result.get('UpdateToken', None)
+        if update_token:
+            self._update_token = update_token
+        return result.get('FirewallStatus', None)
+
+    @Boto3Mixin.aws_error_handler('update firewall description')
+    def _update_firewall_description(self, **params):
+        if self._update_token and 'UpdateToken' not in params:
+            params['UpdateToken'] = self._update_token
+        result = self.client.update_firewall_description(aws_retry=True, **params)
+
+        update_token = result.get('UpdateToken', None)
+        if update_token:
+            self._update_token = update_token
+        return result.get('FirewallName', None)
+
+    @Boto3Mixin.aws_error_handler('update firewall subnet change protection')
+    def _update_subnet_change_protection(self, **params):
+        if self._update_token and 'UpdateToken' not in params:
+            params['UpdateToken'] = self._update_token
+        result = self.client.update_subnet_change_protection(aws_retry=True, **params)
+
+        update_token = result.get('UpdateToken', None)
+        if update_token:
+            self._update_token = update_token
+        return result.get('FirewallName', None)
+
+    @Boto3Mixin.aws_error_handler('update firewall policy change protection')
+    def _update_firewall_policy_change_protection(self, **params):
+        if self._update_token and 'UpdateToken' not in params:
+            params['UpdateToken'] = self._update_token
+        result = self.client.update_firewall_policy_change_protection(aws_retry=True, **params)
+
+        update_token = result.get('UpdateToken', None)
+        if update_token:
+            self._update_token = update_token
+        return result.get('FirewallName', None)
+
+    @Boto3Mixin.aws_error_handler('update firewall deletion protection')
+    def _update_firewall_delete_protection(self, **params):
+        if self._update_token and 'UpdateToken' not in params:
+            params['UpdateToken'] = self._update_token
+        result = self.client.update_firewall_delete_protection(aws_retry=True, **params)
+
+        update_token = result.get('UpdateToken', None)
+        if update_token:
+            self._update_token = update_token
+        return result.get('FirewallName', None)
+
+    @Boto3Mixin.aws_error_handler('associate policy with firewall')
+    def _associate_firewall_policy(self, **params):
+        if self._update_token and 'UpdateToken' not in params:
+            params['UpdateToken'] = self._update_token
+        result = self.client.associate_firewall_policy(aws_retry=True, **params)
+
+        update_token = result.get('UpdateToken', None)
+        if update_token:
+            self._update_token = update_token
+        return result.get('FirewallName', None)
+
+    @Boto3Mixin.aws_error_handler('associate subnets with firewall')
+    def _associate_subnets(self, **params):
+        if self._update_token and 'UpdateToken' not in params:
+            params['UpdateToken'] = self._update_token
+        result = self.client.associate_subnets(aws_retry=True, **params)
+
+        update_token = result.get('UpdateToken', None)
+        if update_token:
+            self._update_token = update_token
+        return result.get('FirewallName', None)
+
+    @Boto3Mixin.aws_error_handler('disassociate subnets from firewall')
+    def _disassociate_subnets(self, **params):
+        if self._update_token and 'UpdateToken' not in params:
+            params['UpdateToken'] = self._update_token
+        result = self.client.disassociate_subnets(aws_retry=True, **params)
+
+        update_token = result.get('UpdateToken', None)
+        if update_token:
+            self._update_token = update_token
+        return result.get('FirewallName', None)
+
+    @Boto3Mixin.aws_error_handler('delete firewall')
+    def _delete_firewall(self, **params):
+        try:
+            result = self.client.delete_firewall(aws_retry=True, **params)
+        except is_boto3_error_code('ResourceNotFoundException'):
+            return None
+
+        return result.get('FirewallPolicyResponse', None)
+
+    @Boto3Mixin.aws_error_handler('firewall to finish deleting')
+    def _wait_firewall_deleted(self, **params):
+        waiter = self.nf_waiter_factory.get_waiter('firewall_deleted')
+        waiter.wait(**params)
+
+    @Boto3Mixin.aws_error_handler('firewall to finish updating')
+    def _wait_firewall_updated(self, **params):
+        waiter = self.nf_waiter_factory.get_waiter('firewall_updated')
+        waiter.wait(**params)
+
+    @Boto3Mixin.aws_error_handler('firewall to become active')
+    def _wait_firewall_active(self, **params):
+        waiter = self.nf_waiter_factory.get_waiter('firewall_active')
         waiter.wait(**params)
 
 
