2e1458558928b9afd7d2e9f45ad5cd0faae7fe94
Alina Buzachis <abuzachis@redhat.com>
2022-05-25 15:21:42 +0200

    Rework and add new features
    Signed-off-by: Alina Buzachis <abuzachis@redhat.com>

--- plugins/modules/rds_cluster_snapshot.py
+++ plugins/modules/rds_cluster_snapshot.py
@@ -13,7 +13,7 @@ module: rds_cluster_snapshot
 version_added: 3.3.0
 short_description: Manage Amazon RDS snapshots of DB clusters
 description:
-  - Creates or deletes RDS snapshots of DB clusters.
+  - Create, modify and delete RDS snapshots of DB clusters.
 options:
   state:
     description:
@@ -38,6 +38,24 @@ options:
       - cluster_id
       - cluster_name
     type: str
+  source_db_cluster_snapshot_identifier:
+     description:
+       - The identifier of the DB cluster snapshot to copy.
+       - If the source snapshot is in the same AWS region as the copy, specify the snapshot's identifier.
+       - If the source snapshot is in a different AWS region as the copy, specify the snapshot's ARN.
+     aliases:
+       - source_id
+       - source_snapshot_id
+     type: str
+  source_region:
+    description:
+      - The region that contains the snapshot to be copied.
+    type: str
+  copy_tags:
+    description:
+      - Whether to copy all tags from I(source_db_cluster_snapshot_identifier) to I(db_snapshot_identifier).
+    type: bool
+    default: False
   wait:
     description:
       - Whether or not to wait for snapshot creation or deletion.
@@ -58,7 +76,7 @@ options:
     default: true
     type: bool
 author:
-    - "Alina Buzachis (@alinabuzachis)"
+    - Alina Buzachis (@alinabuzachis)
 extends_documentation_fragment:
 - amazon.aws.aws
 - amazon.aws.ec2
@@ -74,39 +92,37 @@ EXAMPLES = r'''
   community.aws.rds_cluster_snapshot:
     db_snapshot_identifier: new-cluster-snapshot
     state: absent
+
+- name: Copy snapshot from a different region and copy its tags
+  community.aws.rds_instance_snapshot:
+    id: new-database-snapshot-copy
+    region: us-east-1
+    source_id: "{{ snapshot.db_snapshot_arn }}"
+    source_region: us-east-2
+    copy_tags: yes
 '''
 
 RETURN = r'''
-allocated_storage:
-  description: Specifies the allocated storage size in gibibytes (GiB).
-  returned: always
-  type: int
-  sample: 20
 availability_zone:
   description: Availability zone of the database from which the snapshot was created.
   returned: always
   type: str
   sample: us-west-2a
-cluster_create_time:
-  description: Specifies the time when the DB cluster was created, in Universal Coordinated Time (UTC).
-  returned: always
-  type: str
-  sample: '2019-06-15T10:15:56.221000+00:00'
-db_cluster_identifier:
-  description: Specifies the DB cluster identifier of the DB cluster that this DB cluster snapshot was created from.
-  returned: always
-  type: str
-  sample: ansible-test-16638696
-db_cluster_snapshot_arn:
-  description: Amazon Resource Name for the snapshot.
-  returned: always
-  type: str
-  sample: arn:aws:rds:us-west-2:123456789012:snapshot:ansible-test-16638696-test-snapshot
 db_cluster_snapshot_identifier:
   description: Specifies the identifier for the DB cluster snapshot.
   returned: always
   type: str
   sample: ansible-test-16638696-test-snapshot
+db_cluster_identifier:
+  description: Specifies the DB cluster identifier of the DB cluster that this DB cluster snapshot was created from.
+  returned: always
+  type: str
+  sample: ansible-test-16638696
+snapshot_create_time:
+  description: Provides the time when the snapshot was taken, in Universal Coordinated Time (UTC).
+  returned: always
+  type: str
+  sample: '2019-06-15T10:46:23.776000+00:00'
 engine:
   description: Specifies the name of the database engine for this DB cluster snapshot.
   returned: always
@@ -117,53 +133,77 @@ engine_mode:
   returned: always
   type: str
   sample: "5.6.mysql_aurora.1.22.5"
-engine_version:
-  description: Version of the cluster from which the snapshot was created.
-  returned: always
-  type: str
-  sample: "5.6.mysql_aurora.1.22.5"
-iam_database_authentication_enabled:
-  description: Whether IAM database authentication is enabled.
-  returned: always
-  type: bool
-  sample: false
-license_model:
-  description: Provides the license model information for this DB cluster snapshot.
-  returned: always
-  type: str
-  sample: general-public-license
-master_username:
-  description: Provides the master username for this DB cluster snapshot.
-  returned: always
-  type: str
-  sample: test
-percent_progress:
-  description: Specifies the percentage of the estimated data that has been transferred.
+allocated_storage:
+  description: Specifies the allocated storage size in gibibytes (GiB).
   returned: always
   type: int
-  sample: 100
-port:
-  description: Port on which the database is listening.
-  returned: always
-  type: int
-  sample: 3306
-snapshot_create_time:
-  description: Provides the time when the snapshot was taken, in Universal Coordinated Time (UTC).
-  returned: always
-  type: str
-  sample: '2019-06-15T10:46:23.776000+00:00'
-snapshot_type:
-  description: How the snapshot was created (always manual for this module!).
-  returned: always
-  type: str
-  sample: manual
+  sample: 20
 status:
   description: Specifies the status of this DB cluster snapshot.
   returned: always
   type: str
   sample: available
+port:
+  description: Port on which the database is listening.
+  returned: always
+  type: int
+  sample: 3306
+vpc_id:
+  description: ID of the VPC in which the DB lives.
+  returned: always
+  type: str
+  sample: vpc-09ff232e222710ae0
+cluster_create_time:
+  description: Specifies the time when the DB cluster was created, in Universal Coordinated Time (UTC).
+  returned: always
+  type: str
+  sample: '2019-06-15T10:15:56.221000+00:00'
+master_username:
+  description: Provides the master username for this DB cluster snapshot.
+  returned: always
+  type: str
+  sample: test
+engine_version:
+  description: Version of the cluster from which the snapshot was created.
+  returned: always
+  type: str
+  sample: "5.6.mysql_aurora.1.22.5"
+license_model:
+  description: Provides the license model information for this DB cluster snapshot.
+  returned: always
+  type: str
+  sample: general-public-license
+snapshot_type:
+  description: How the snapshot was created (always manual for this module!).
+  returned: always
+  type: str
+  sample: manual
+percent_progress:
+  description: Specifies the percentage of the estimated data that has been transferred.
+  returned: always
+  type: int
+  sample: 100
 storage_encrypted:
   description: Specifies whether the DB cluster snapshot is encrypted.
+  returned: always
+  type: bool
+  sample: false
+kms_key_id:
+  description: The Amazon Web Services KMS key identifier is the key ARN, key ID, alias ARN, or alias name for the KMS key.
+  returned: always
+  type: str
+db_cluster_snapshot_arn:
+  description: Amazon Resource Name for the snapshot.
+  returned: always
+  type: str
+  sample: arn:aws:rds:us-west-2:123456789012:snapshot:ansible-test-16638696-test-snapshot
+source_db_cluster_snapshot_arn:
+  description: If the DB cluster snapshot was copied from a source DB cluster snapshot, the Amazon Resource Name (ARN) for the source DB cluster snapshot, otherwise, a null value.
+  returned: always
+  type: str
+  sample: null
+iam_database_authentication_enabled:
+  description: Whether IAM database authentication is enabled.
   returned: always
   type: bool
   sample: false
@@ -177,11 +217,6 @@ tags:
   returned: always
   type: complex
   contains: {}
-vpc_id:
-  description: ID of the VPC in which the DB lives.
-  returned: always
-  type: str
-  sample: vpc-09ff232e222710ae0
 '''
 
 try:
@@ -197,23 +232,34 @@ from ansible_collections.amazon.aws.plugins.module_utils.ec2 import ansible_dict
 from ansible_collections.amazon.aws.plugins.module_utils.rds import get_tags
 from ansible_collections.amazon.aws.plugins.module_utils.rds import ensure_tags
 from ansible_collections.amazon.aws.plugins.module_utils.rds import call_method
+from ansible_collections.amazon.aws.plugins.module_utils.core import get_boto3_client_method_parameters
+from ansible_collections.amazon.aws.plugins.module_utils.rds import get_rds_method_attribute
+from ansible_collections.amazon.aws.plugins.module_utils.rds import arg_spec_to_rds_params
 
 
 def get_snapshot(snapshot_id):
     try:
-        response = client.describe_db_cluster_snapshots(DBClusterSnapshotIdentifier=snapshot_id)
-    except is_boto3_error_code("DBClusterSnapshotNotFoundFault"):
-        return None
-    except is_boto3_error_code("DBClusterSnapshotNotFound"):  # pylint: disable=duplicate-except
-        return None
+        snapshot = client.describe_db_cluster_snapshots(DBClusterSnapshotIdentifier=snapshot_id)["DBClusterSnapshots"][0]
+        snapshot["Tags"] = get_tags(client, module, snapshot["DBClusterSnapshotArn"])
+    except is_boto3_error_code("DBClusterSnapshotNotFound"):
+        return {}
     except (botocore.exceptions.BotoCoreError, botocore.exceptions.ClientError) as e:  # pylint: disable=duplicate-except
         module.fail_json_aws(e, msg="Couldn't get snapshot {0}".format(snapshot_id))
-    return response["DBClusterSnapshots"][0]
+    return snapshot
 
 
-def fetch_tags(snapshot):
-    snapshot["Tags"] = get_tags(client, module, snapshot["DBClusterSnapshotArn"])
-    return camel_dict_to_snake_dict(snapshot, ignore_list=["Tags"])
+def get_parameters(parameters, method_name):
+    if method_name == 'copy_db_cluster_snapshot':
+        parameters['TargetDBClusterSnapshotIdentifier'] = module.params['db_cluster_snapshot_identifier']
+
+    required_options = get_boto3_client_method_parameters(client, method_name, required=True)
+    if any(parameters.get(k) is None for k in required_options):
+        module.fail_json(msg='To {0} requires the parameters: {1}'.format(
+            get_rds_method_attribute(method_name, module).operation_description, required_options))
+    options = get_boto3_client_method_parameters(client, method_name)
+    parameters = dict((k, v) for k, v in parameters.items() if k in options and v is not None)
+
+    return parameters
 
 
 def ensure_snapshot_absent():
@@ -223,42 +269,67 @@ def ensure_snapshot_absent():
 
     snapshot = get_snapshot(snapshot_name)
     if not snapshot:
-        return dict(changed=changed)
+        module.exit_json(changed=changed)
     elif snapshot and snapshot["Status"] != "deleting":
         snapshot, changed = call_method(client, module, "delete_db_cluster_snapshot", params)
 
-    return dict(changed=changed)
+    module.exit_json(changed=changed)
 
 
-def ensure_snapshot_present():
-    db_cluster_identifier = module.params.get("db_cluster_identifier")
+def copy_snapshot(params):
+    changed = False
+    snapshot_id = module.params.get('db_cluster_snapshot_identifier')
+    snapshot = get_snapshot(snapshot_id)
+
+    if not snapshot:
+        method_params = get_parameters(params, 'copy_db_cluster_snapshot')
+        if method_params.get('Tags'):
+            method_params['Tags'] = ansible_dict_to_boto3_tag_list(method_params['Tags'])
+        result, changed = call_method(client, module, 'copy_db__cluster_snapshot', method_params)
+
+    return changed
+
+
+def ensure_snapshot_present(params):
+    source_id = module.params.get('source_db_cluster_snapshot_identifier')
     snapshot_name = module.params.get("db_snapshot_identifier")
     changed = False
 
+    # Copy snapshot
+    if source_id:
+        changed |= copy_snapshot(params)
+
+    # Create snapshot
+    elif not snapshot:
+        changed |= create_snapshot(params)
+
+    # Snapshot exists and we're not creating a copy - modify exising snapshot
+    else:
+        changed |= modify_snapshot()
+
     snapshot = get_snapshot(snapshot_name)
+    module.exit_json(changed=changed, **camel_dict_to_snake_dict(snapshot, ignore_list=['Tags']))
 
-    if not snapshot:
-        params = {
-            "DBClusterSnapshotIdentifier": snapshot_name,
-            "DBClusterIdentifier": db_cluster_identifier
-        }
-        if module.params.get("tags"):
-            params['Tags'] = ansible_dict_to_boto3_tag_list(module.params.get("tags"))
-        _result, changed = call_method(client, module, "create_db_cluster_snapshot", params)
 
-        if module.check_mode:
-            return dict(changed=changed)
+def create_snapshot(params):
+    method_params = get_parameters(params, 'create_db_cluster_snapshot')
+    if method_params.get('Tags'):
+        method_params['Tags'] = ansible_dict_to_boto3_tag_list(method_params['Tags'])
+    snapshot, changed = call_method(client, module, 'create_db_cluster_snapshot', method_params)
 
-        return dict(changed=changed, **fetch_tags(get_snapshot(snapshot_name)))
+    return changed
 
-    existing_tags = get_tags(client, module, snapshot["DBClusterSnapshotArn"])
-    changed |= ensure_tags(client, module, snapshot["DBClusterSnapshotArn"], existing_tags,
-                           module.params["tags"], module.params["purge_tags"])
 
-    if module.check_mode:
-        return dict(changed=changed)
+def modify_snapshot():
+    # TODO - add other modifications aside from purely tags
+    changed = False
+    snapshot_id = module.params.get('db_cluster_snapshot_identifier')
+    snapshot = get_snapshot(snapshot_id)
 
-    return dict(changed=changed, **fetch_tags(get_snapshot(snapshot_name)))
+    if module.params.get('tags'):
+        changed |= ensure_tags(client, module, snapshot['DBClusterSnapshotArn'], snapshot['Tags'], module.params['tags'], module.params['purge_tags'])
+
+    return changed
 
 
 def main():
@@ -266,18 +337,20 @@ def main():
     global module
 
     argument_spec = dict(
-        state=dict(choices=['present', 'absent'], default='present'),
-        db_snapshot_identifier=dict(aliases=['id', 'snapshot_id', 'snapshot_name'], required=True),
-        db_cluster_identifier=dict(aliases=['cluster_id', 'cluster_name']),
+        state=dict(type='str', choices=['present', 'absent'], default='present'),
+        db_snapshot_identifier=dict(type='str', aliases=['id', 'snapshot_id', 'snapshot_name'], required=True),
+        db_cluster_identifier=dict(type='str', aliases=['cluster_id', 'cluster_name']),
+        source_db_cluster_snapshot_identifier=dict(type='str', aliases=['source_id', 'source_snapshot_id']),
         wait=dict(type='bool', default=False),
         wait_timeout=dict(type='int', default=300),
         tags=dict(type='dict'),
         purge_tags=dict(type='bool', default=True),
+        copy_tags=dict(type='bool', default=False),
+        source_region=dict(type='str'),
     )
 
     module = AnsibleAWSModule(
         argument_spec=argument_spec,
-        required_if=[['state', 'present', ['db_cluster_identifier']]],
         supports_check_mode=True,
     )
 
@@ -287,14 +360,13 @@ def main():
     except (botocore.exceptions.ClientError, botocore.exceptions.BotoCoreError) as e:
         module.fail_json_aws(e, msg="Failed to connect to AWS.")
 
-    result = {}
+    state = module.params.get("state")
 
-    if module.params["state"] == "absent":
-        result = ensure_snapshot_absent()
-    else:
-        result = ensure_snapshot_present()
-
-    module.exit_json(**result)
+    if state == "absent":
+        ensure_snapshot_absent()
+    elif state == "present":
+        params = arg_spec_to_rds_params(dict((k, module.params[k]) for k in module.params if k in argument_spec))
+        ensure_snapshot_present(params)
 
 
 if __name__ == '__main__':
