bcc447aefeefddfa84fb60661bd9230b00f7405e
Mark Chappell <mchappel@redhat.com>
2022-06-07 09:38:08 +0200

    Partial backport of aws_codebuild - Add resource_tags parameter and deprecate tags (#1221)
    fix bug with inconsistent "changed" state due to tag order not being guaranteed
    
    ISSUE TYPE
    
    Bugfix Pull Request
    Feature Pull Request
    
    COMPONENT NAME
    aws_codebuild
    ADDITIONAL INFORMATION
    The (boto3) tags format in the return value when describing a project makes no guarantees about the order it'll return the key/value pairs.  As such, when multiple tags were set the naïve original == new comparison would sporadically return "changed" when no change had occurred.
    
    Reviewed-by: Joseph Torcasso <None>
    Reviewed-by: Mark Chappell <None>
    (cherry picked from commit 20f2afd75e506d90a72fb27e4cba3bb09f707494)

--- plugins/modules/aws_codebuild.py
+++ plugins/modules/aws_codebuild.py
@@ -285,8 +285,13 @@ project:
       sample: "2018-04-17T16:56:03.245000+02:00"
 '''
 
-from ansible_collections.amazon.aws.plugins.module_utils.core import AnsibleAWSModule, get_boto3_client_method_parameters
-from ansible_collections.amazon.aws.plugins.module_utils.ec2 import camel_dict_to_snake_dict, snake_dict_to_camel_dict
+from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
+from ansible.module_utils.common.dict_transformations import snake_dict_to_camel_dict
+
+from ansible_collections.amazon.aws.plugins.module_utils.core import AnsibleAWSModule
+from ansible_collections.amazon.aws.plugins.module_utils.core import get_boto3_client_method_parameters
+from ansible_collections.amazon.aws.plugins.module_utils.tagging import ansible_dict_to_boto3_tag_list
+from ansible_collections.amazon.aws.plugins.module_utils.tagging import boto3_tag_list_to_ansible_dict
 
 
 try:
@@ -318,11 +323,14 @@ def create_or_update_project(client, params, module):
         # Prep both dicts for sensible change comparison:
         found_project.pop('lastModified')
         updated_project.pop('lastModified')
-        if 'tags' not in updated_project:
-            updated_project['tags'] = []
+        updated_tags = updated_project.pop('tags', [])
+        found_project['ResourceTags'] = boto3_tag_list_to_ansible_dict(found_tags)
+        updated_project['ResourceTags'] = boto3_tag_list_to_ansible_dict(updated_tags)
 
         if updated_project != found_project:
             changed = True
+        update_project.pop('ResourceTags')
+        updated_project['tags'] = updated_tags
         return resp, changed
     # Or create new project:
     try:
