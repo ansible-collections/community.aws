fe4ac90b0d5aa0eec7a1955a46f16cd4c329bf8f
Mark Chappell <mchappel@redhat.com>
2022-07-04 10:10:24 +0200

    Tests and meta

--- plugins/modules/autoscaling_complete_lifecycle_action.py
+++ plugins/modules/autoscaling_complete_lifecycle_action.py
@@ -7,18 +7,17 @@ __metaclass__ = type
 
 DOCUMENTATION = '''
 ---
-module: aws_asg_complete_lifecycle_action
+module: autoscaling_complete_lifecycle_action
 short_description: Completes the lifecycle action of an instance
 description:
-  - Used to complete the lifecycle action for the specified instance with the specified result
-version_added: "1.0.0"
-requirements: [ boto3 ]
+  - Used to complete the lifecycle action for the specified instance with the specified result.
+version_added: "4.1.0"
 author:
-    - Saleh Abbas (@salehabbas) <saleh.abbas@thetradedesk.com>
+  - Saleh Abbas (@salehabbas) <saleh.abbas@thetradedesk.com>
 options:
   asg_name:
     description:
-      - The name of the Auto Scaling group which the instance belongs to.
+      - The name of the Auto Scaling Group which the instance belongs to.
     type: str
     required: true
   lifecycle_hook_name:
@@ -28,7 +27,8 @@ options:
     required: true
   lifecycle_action_result:
     description:
-      - The action for the lifecycle hook to take. It can be either CONTINUE or ABANDON.
+      - The action for the lifecycle hook to take.
+    choices: ['CONTINUE', 'ABANDON']
     type: str
     required: true
   instance_id:
@@ -37,8 +37,8 @@ options:
     type: str
     required: true
 extends_documentation_fragment:
-    - amazon.aws.aws
-    - amazon.aws.ec2
+  - amazon.aws.aws
+  - amazon.aws.ec2
 '''
 
 EXAMPLES = '''
@@ -60,45 +60,39 @@ status:
     sample: ["OK"]
 '''
 
-import re
-
 try:
     import botocore
 except ImportError:
     pass  # caught by AnsibleAWSModule
 
 from ansible_collections.amazon.aws.plugins.module_utils.core import AnsibleAWSModule
-from ansible_collections.amazon.aws.plugins.module_utils.ec2 import get_aws_connection_info, boto3_conn, HAS_BOTO3
+
 
 def main():
     argument_spec = dict(
         asg_name=dict(required=True, type='str'),
         lifecycle_hook_name=dict(required=True, type='str'),
-        lifecycle_action_result=dict(required=True, type='str'),
+        lifecycle_action_result=dict(required=True, type='str', choices=['CONTINUE', 'ABANDON']),
         instance_id=dict(required=True, type='str')
     )
 
     module = AnsibleAWSModule(argument_spec=argument_spec)
-    if not HAS_BOTO3:
-        module.fail_json(msg='boto3 required for this module')
 
     asg_name = module.params.get('asg_name')
     lifecycle_hook_name = module.params.get('lifecycle_hook_name')
     lifecycle_action_result = module.params.get('lifecycle_action_result')
     instance_id = module.params.get('instance_id')
 
+    autoscaling = module.client('autoscaling')
     try:
-        region, ec2_url, aws_connect_kwargs = get_aws_connection_info(module, boto3=True)
-        autoscaling = boto3_conn(module, conn_type='client', resource='autoscaling', region=region, endpoint=ec2_url, **aws_connect_kwargs)
+        results = autoscaling.complete_lifecycle_action(
+            LifecycleHookName=lifecycle_hook_name,
+            AutoScalingGroupName=asg_name,
+            LifecycleActionResult=lifecycle_action_result,
+            InstanceId=instance_id
+        )
     except (botocore.exceptions.ClientError, botocore.exceptions.BotoCoreError) as e:
         module.fail_json_aws(e, msg="Failed to completes the lifecycle action")
-
-    results = autoscaling.complete_lifecycle_action(
-        LifecycleHookName=lifecycle_hook_name,
-        AutoScalingGroupName=asg_name,
-        LifecycleActionResult=lifecycle_action_result,
-        InstanceId=instance_id
-    )
 
     module.exit_json(results=results)
 
