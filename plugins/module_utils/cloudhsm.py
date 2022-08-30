0bb74ff249e2c6c59a4a8225ab39735fefdac0a3
Armen Martirosyan <armartirosyan@gmail.com>
2022-08-30 14:44:27 -0700

    CloudHSM modules

---
+++ plugins/module_utils/cloudhsm.py
@@ -0,0 +1,276 @@
+# Copyright: (c) 2022, TachTech <info@tachtech.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+
+try:
+    import botocore
+except ImportError:
+    pass  # Handled by AnsibleAWSModule
+
+import os
+
+from ansible_collections.amazon.aws.plugins.module_utils.tagging import (
+    ansible_dict_to_boto3_tag_list,
+)
+
+STATE = ["present", "absent"]
+
+CLUSTER_STATES = [
+    "CREATE_IN_PROGRESS",
+    "UNINITIALIZED",
+    "INITIALIZE_IN_PROGRESS",
+    "INITIALIZED",
+    "ACTIVE",
+    "UPDATE_IN_PROGRESS",
+    "DELETE_IN_PROGRESS",
+    "DELETED",
+    "DEGRADED",
+]
+
+HSM_STATES = [
+    "CREATE_IN_PROGRESS",
+    "ACTIVE",
+    "DEGRADED",
+    "DELETE_IN_PROGRESS",
+    "DELETED",
+]
+
+
+class CloudHsmCluster:
+    """Handles CloudHSM Clusters"""
+
+    def __init__(self, module):
+        self.module = module
+        try:
+            self.client = module.client("cloudhsmv2")
+        except (
+            botocore.exceptions.ClientError,
+            botocore.exceptions.BotoCoreError,
+        ) as e:
+            self.module.fail_json(e, msg="Failed to connect to AWS")
+
+    def describe_cluster(self):
+        """Returns cluster information from the provided data.
+
+        Returns:
+            (list): Cluster information
+        """
+        filter_dict = {"Filters": {}}
+        cluster_ids = self.module.params.get("cluster_id", [])
+        states = self.module.params.get("state", [])
+        name = self.module.params.get("name")
+        cluster_data = []
+        if isinstance(states, str):
+            states = []
+        try:
+            if cluster_ids:
+                if isinstance(cluster_ids, str):
+                    cluster_ids = cluster_ids.split()
+                for cluster_id in cluster_ids:
+                    filter_dict["Filters"].update({"clusterIds": [cluster_id]})
+                    cluster_data.extend(
+                        self.client.describe_clusters(**filter_dict)["Clusters"]
+                    )
+            if states:
+                for state in states:
+                    filter_dict["Filters"].update({"states": [state]})
+                    cluster_data.extend(
+                        self.client.describe_clusters(**filter_dict)["Clusters"]
+                    )
+            if not cluster_ids and not states:
+                cluster_data.extend(
+                    self.client.describe_clusters(**filter_dict)["Clusters"]
+                )
+        except Exception as catch_all:
+            self.module.fail_json(
+                msg=f"Exception raised while describing cluster: {str(catch_all)}"
+            )
+        if name:
+            clusters = []
+            for cluster in cluster_data:
+                for tag in cluster["TagList"]:
+                    if tag["Key"] == "name" and tag["Value"] == name:
+                        clusters.append(cluster)
+            return clusters
+        return cluster_data
+
+    def create_cluster(self):
+        """Creates CloudHSM Cluster
+
+        Returns:
+            (dict): Created cluster information
+        """
+        cluster_body = {
+            "BackupRetentionPolicy": {
+                "Type": "DAYS",
+                "Value": self.module.params["backup_retention_days"],
+            },
+            "HsmType": "hsm1.medium",
+            "SubnetIds": self.module.params["subnet_ids"],
+            "TagList": [
+                {"Key": "name", "Value": self.module.params["name"]},
+            ],
+        }
+        if self.module.params.get("tags"):
+            cluster_body["TagList"].extend(
+                ansible_dict_to_boto3_tag_list(self.module.params.get("tags"))
+            )
+        try:
+            return self.client.create_cluster(**cluster_body)["Cluster"]
+        except Exception as catch_all:
+            self.module.fail_json(
+                msg=f"Exception raised while creating cluster '{self.module.params['name']}': {str(catch_all)}"
+            )
+
+    def delete_cluster(self, cluster_id):
+        """Deletes CloudHsm Cluster
+
+        Args:
+            cluster_id (str): CloudHSM Cluster ID
+
+        Returns:
+            (dict): Information about deleted cluster
+        """
+        try:
+            return self.client.delete_cluster(ClusterId=cluster_id)
+        except Exception as catch_all:
+            self.module.fail_json(
+                msg=f"Exception raised while deleting cluster '{self.module.params['name']}': {str(catch_all)}"
+            )
+
+    def initialize(self, cluster_id):
+        """Initializes the HSM Cluster.
+
+        Args:
+            cluster_id (str): Cluster_ID to initialize
+        """
+        init_body = {"ClusterId": cluster_id}
+        signed_cert = self.module.params["signed_cert"]
+        trust_anchor = self.module.params["trust_anchor"]
+        if os.path.isfile(signed_cert):
+            with open(signed_cert) as _file:
+                init_body["SignedCert"] = _file.read()
+        else:
+            init_body["SignedCert"] = signed_cert
+        if os.path.isfile(trust_anchor):
+            with open(trust_anchor) as _file:
+                init_body["TrustAnchor"] = _file.read()
+        else:
+            init_body["TrustAnchor"] = trust_anchor
+        try:
+            self.client.initialize_cluster(**init_body)
+            return True
+        except Exception as catch_all:
+            self.module.fail_json(
+                msg=f"Exception raised while initializing cluster: {str(catch_all)}"
+            )
+
+    def create_hsm(self, request_body):
+        """Creates HSM Devices
+
+        Args:
+            request_body (dict): Dictionary containing information to create new HSM
+
+        Returns:
+            dict[str, Any]: Returns dictionary of data
+        """
+        try:
+            return self.client.create_hsm(**request_body)["Hsm"]
+        except Exception as catch_all:
+            self.module.fail_json(
+                msg=f"Exception raised while creating HSM: {catch_all}"
+            )
+
+    def delete_hsm(self, request_body):
+        """Deletes HSM Devices
+
+        Args:
+            request_body (dict): Dictionary containing information to delete HSM
+
+        Returns:
+            None
+        """
+        try:
+            self.client.delete_hsm(**request_body)
+        except Exception as catch_all:
+            self.module.json_fail(f"Exception raised while deleting HSM: {catch_all}")
+
+    def describe_hsm(self, extend_search=False):
+        """Returns all available HSMs for particular HSM Cluster.
+
+        Args:
+            extend_search (bool): Do extended search for particular HSM. Defaults to False.
+
+        Returns:
+            (list): List of HSM if any available
+        """
+
+        def _find_hsm(key_name, expected_value):
+            """Find a particular HSM
+
+            Args:
+                key_name (str): HSM key names 'EniId', 'EniIp', 'HsmId', 'State'
+                expected_value (str): Value to check for match
+
+            Returns:
+                Union[list[Any], list[None]]: If found desired HSM return as list, if not return empty list
+            """
+            hsms_to_return = []
+            for hsm in hsms:
+                if hsm[key_name] == expected_value:
+                    hsms_to_return.append(hsm)
+            return hsms_to_return
+
+        hsm_cluster = self.describe_cluster()
+        hsms_to_return = []
+        # TODO: Find better logic on how to do this
+        if hsm_cluster:
+            hsms = [
+                hsm for cluster in hsm_cluster for hsm in cluster["Hsms"]
+            ]  # All HSMs across all HSM Clusters
+            temp_hsms = []
+            eni_id = self.module.params.get("eni_id")
+            hsm_id = self.module.params.get("hsm_id")
+            eni_ip = self.module.params.get("eni_ip")
+            state = self.module.params.get("state")
+            if isinstance(state, str):
+                state = []
+            if not extend_search:
+                return hsms
+            if not hsms:
+                return hsms
+            if not eni_id and not state and not hsm_id and not eni_ip:
+                return hsms
+            filters = {
+                "EniId": eni_id,
+                "State": state,
+                "EniIp": eni_ip,
+                "HsmId": hsm_id,
+            }
+
+            # Example Data:
+            # filters = {
+            #     "EniId": None,
+            #     "State": ["ACTIVE", "CREATE_IN_PROGRESS"],
+            #     "EniIp": ["192.168.0.1"],
+            #     "HsmId": None
+            #     }
+            for key, value in filters.items():
+                for _value in value:
+                    temp_hsms.extend(_find_hsm(key, _value))
+            if temp_hsms:
+                for hsm in temp_hsms:
+                    if hsm not in hsms_to_return:
+                        hsms_to_return.append(hsm)
+            return hsms_to_return
+        elif not extend_search:
+            self.module.fail_json(
+                msg=f"Unable to find CloudHSM Cluster with given argument \
+                    '{'name: '+self.module.params['name'] if self.module.params['name'] else 'cluster_id:'+self.module.params['cluster_id'] }'. \
+                        Please check the value and re-run the task. {hsm_cluster}"
+            )
+        return []
---
+++ plugins/modules/cloudhsm_hsm.py
@@ -0,0 +1,312 @@
+#!/usr/bin/python
+
+# Copyright: (c) 2022, TachTech <info@tachtech.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+DOCUMENTATION = """
+---
+module: cloudhsm_hsm
+short_description: Create HSM Device in AWS.
+author:
+    - Armen Martirosyan (@armartirosyan)
+requirements:
+    - boto3
+description:
+    - Create HSM Device(s) in AWS's HSM Cluster.
+options:
+    availability_zone:
+        description:
+            - Availability Zone where the HSM will be created
+        type: str
+        required: false
+    cluster_id:
+        description:
+            - The HSM cluster's identifier.
+        type: str
+        required: false
+    ip_address:
+        description:
+            - IP Address of the HSM device.
+        type: list
+        required: false
+        elements: str
+    state:
+        description:
+            - The state of the HSM Device. If present, the device will be created, if absent, the device will be removed.
+        type: str
+        required: true
+        choices:
+            - present
+            - absent
+    name:
+        description:
+            - The name of the cluster
+        type: str
+        required: false
+    count:
+        description:
+            - Number of HSM devices that need to be created.
+            - The value cannot be greater than 3.
+        type: int
+        required: false
+        default: 1
+    eni_id:
+        description:
+            - Elastic network interface (ENI) identifier of the HSM
+        type: list
+        elements: str
+        required: false
+    eni_ip:
+        description:
+            - Elastic network interface (ENI) IP address of the HSM
+        type: list
+        elements: str
+        required: false
+    hsm_id:
+        description:
+            - The identifier of the HSM
+        type: list
+        elements: str
+        required: false
+extends_documentation_fragment:
+- amazon.aws.aws
+- amazon.aws.ec2
+"""
+
+
+EXAMPLES = """
+# Note: These examples do not set authentication details, see the AWS Guide for details
+
+- name: "Create an HSM Device"
+  community.aws.cloudhsm_hsm:
+    availability_zone: us-west-2b
+    cluster_id: cluster_a3231231
+    count: 1
+    state: present
+
+
+- name: "Create an HSM Device with IP"
+  community.aws.cloudhsm_hsm:
+    availability_zone: us-west-2b
+    cluster_id: cluster_a3231231
+    ip_address: 192.168.0.1
+    count: 1
+    state: present
+
+- name: "Create Two HSM Devices with IP"
+  community.aws.cloudhsm_hsm:
+    availability_zone: us-west-2b
+    cluster_id: cluster_a3231231
+    ip_address:
+      - 192.168.0.1  # IP address of the first HSM Device
+      - 192.168.0.2  # IP address of the second HSM Device
+    count: 2
+    state: present
+
+- name: "Add Second HSM Devices to the Existing One"
+  community.aws.cloudhsm_hsm:
+    availability_zone: us-west-2b
+    cluster_id: cluster_a3231231
+    count: 2
+    state: present
+
+- name: "Remove all HSM Devices"
+  community.aws.cloudhsm_hsm:
+    cluster_id: cluster_a3231231
+    state: absent
+
+- name: "Remove HSM Device Using HSM ID"
+  community.aws.cloudhsm_hsm:
+    cluster_id: cluster_a3231231
+    hsm_id: hsm_asdasd123123
+    state: absent
+
+- name: "Remove HSM Device Using ENI ID"
+  community.aws.cloudhsm_hsm:
+    cluster_id: cluster_a3231231
+    eni_id: eni_123asd123
+    state: absent
+
+- name: "Remove HSM Device Using ENI IP"
+  community.aws.cloudhsm_hsm:
+    cluster_id: cluster_a3231231
+    eni_ip: 192.168.0.1
+    state: absent
+"""
+
+RETURN = """
+changed:
+  description: Boolean that is true if the command changed the state.
+  returned: always
+  type: bool
+  sample: True
+data:
+  description: HSM information returned by the AWS.
+  returned: always
+  type: list
+  sample:
+    - availability_zone: us-east-2b
+      cluster_id: cluster-test4
+      eni_id: eni-test
+      eni_ip: 127.0.0.1
+      hsm_id: hsm-test
+      state: ACTIVE
+      state_message: HSM created
+      subnet_id: subnet-test
+"""
+
+
+from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
+from ansible_collections.amazon.aws.plugins.module_utils.core import AnsibleAWSModule
+from ansible_collections.community.aws.plugins.module_utils.cloudhsm import (
+    CloudHsmCluster,
+)
+
+
+def main():
+    """Main function for the module."""
+
+    argument_spec = dict(
+        state=dict(required=True, choices=["present", "absent"], type="str"),
+        availability_zone=dict(required=False, type="str"),
+        cluster_id=dict(required=False, type="str"),
+        name=dict(required=False, type="str"),
+        count=dict(required=False, type="int", default=1),
+        ip_address=dict(required=False, type="list", default=[], elements="str"),
+        eni_id=dict(required=False, type="list", default=[], elements="str"),
+        eni_ip=dict(required=False, type="list", default=[], elements="str"),
+        hsm_id=dict(required=False, type="list", default=[], elements="str"),
+    )
+    required_if = [
+        ("state", "present", ("name", "cluster_id"), True),
+        ("state", "present", ("availability_zone",)),
+        ("state", "absent", ("cluster_id", "name"), True),
+    ]
+
+    module = AnsibleAWSModule(
+        argument_spec=argument_spec,
+        supports_check_mode=True,
+        required_if=required_if,
+    )
+    cluster_mgr = CloudHsmCluster(module)
+    ip_addr = cluster_mgr.module.params.get("ip_address", [])
+    count = cluster_mgr.module.params["count"]
+    if count < 1 or count > 3:
+        cluster_mgr.module.fail_json(
+            msg=f"'count' value cannot be less than 1 and more than 3. Provided: {count}"
+        )
+    if ip_addr and len(ip_addr) != count:
+        cluster_mgr.module.fail_json(
+            msg="Number of provided IP Addresses must be equal to the 'count' value."
+        )
+
+    existing_hsms = cluster_mgr.describe_hsm()
+    existing_cluster = cluster_mgr.describe_cluster()
+    results = dict(changed=False, data=[])
+    if module.params["state"] == "present":
+        if len(existing_hsms) == count or len(existing_hsms) > count:
+            for hsm in existing_hsms:
+                results["data"].append(camel_dict_to_snake_dict(hsm))
+        else:
+            if not module.check_mode:
+                # doesn't exist. create it
+                hsm_body = {
+                    "ClusterId": existing_cluster[0]["ClusterId"],
+                    "AvailabilityZone": cluster_mgr.module.params["availability_zone"],
+                }
+                if count == 1:
+                    if ip_addr:
+                        hsm_body.update({"IpAddress": ip_addr[0]})
+                    results["data"].append(
+                        camel_dict_to_snake_dict(cluster_mgr.create_hsm(hsm_body))
+                    )
+                else:
+                    if ip_addr:
+                        for ip_addr in ip_addr[count - (count - len(existing_hsms)):]:
+                            hsm_body.update({"IpAddress": ip_addr})
+                            results["data"].append(
+                                camel_dict_to_snake_dict(
+                                    cluster_mgr.create_hsm(hsm_body)
+                                )
+                            )
+                        results["changed"] = True
+                    else:
+                        for num in range(0, count - len(existing_hsms)):
+                            results["data"].append(
+                                camel_dict_to_snake_dict(
+                                    cluster_mgr.create_hsm(hsm_body)
+                                )
+                            )
+                        results["changed"] = True
+            else:
+                hsm_data = {
+                    "AvailabilityZone": cluster_mgr.module.params["availability_zone"],
+                    "ClusterId": existing_cluster[0]["ClusterId"],
+                    "SubnetId": "subnet-check-mode-on",
+                    "State": "ACTIVE",
+                    "StateMessage": "HSM created.",
+                }
+                for i in range(0, count - len(existing_hsms)):
+                    hsm_data.update(
+                        {
+                            "EniIp": f"127.0.0.{i}",
+                            "HsmId": f"hsm-check-mode-on-{i}",
+                            "EniId": f"eni-check-mode-on-{i}",
+                        }
+                    )
+                    results["data"].append(camel_dict_to_snake_dict(hsm_data))
+                results["changed"] = True
+
+    # delete the cluster
+    elif module.params["state"] == "absent":
+        count = cluster_mgr.module.params["count"]
+        eni_id = cluster_mgr.module.params.get("eni_id")
+        eni_ip = cluster_mgr.module.params.get("eni_ip")
+        hsm_id = cluster_mgr.module.params.get("hsm_id")
+        hsm_body = {
+            "ClusterId": existing_cluster[0]["ClusterId"],
+        }
+        if not cluster_mgr.module.check_mode:
+            if not eni_id and not eni_ip and not hsm_id:
+                # If no information is provided, delete all HSMs
+                hsm_ids_to_remove = [
+                    hsm["HsmId"]
+                    for index, hsm in enumerate(existing_hsms)
+                    if index <= count and existing_cluster[0]["Hsms"]
+                ]
+                if not hsm_ids_to_remove:
+                    results[
+                        "msg"
+                    ] = f"CloudHSM Cluster ID '{existing_cluster[0]['ClusterId']}' does not contain any HSMs."
+                else:
+                    for hsm_id in hsm_ids_to_remove:
+                        hsm_body.update({"HsmId": hsm_id})
+                        cluster_mgr.delete_hsm(hsm_body)
+                    results["changed"] = True
+            elif hsm_id:
+                for hsm in hsm_id:
+                    hsm_body.update({"HsmId": hsm})
+                    cluster_mgr.delete_hsm(hsm_body)
+                results["changed"] = True
+            elif eni_ip:
+                for eni in eni_ip:
+                    hsm_body.update({"EniIp": eni})
+                    cluster_mgr.delete_hsm(hsm_body)
+                results["changed"] = True
+            elif eni_id:
+                for eni in eni_id:
+                    hsm_body.update({"EniId": eni})
+                    cluster_mgr.delete_hsm(hsm_body)
+                results["changed"] = True
+        else:
+            results["changed"] = True
+
+    cluster_mgr.module.exit_json(**results)
+
+
+if __name__ == "__main__":
+    main()
