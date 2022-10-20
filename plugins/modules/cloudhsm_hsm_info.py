16c057885caea9a6b802ad59c0d971ce44341c59
Armen Martirosyan <armartirosyan@gmail.com>
2022-10-20 08:24:15 -0700

    Changed supports_check_mode from False to True

--- plugins/modules/cloudhsm_hsm_info.py
+++ plugins/modules/cloudhsm_hsm_info.py
@@ -141,7 +141,7 @@ def main():
 
     module = AnsibleAWSModule(
         argument_spec=argument_spec,
-        supports_check_mode=False,
+        supports_check_mode=True,
     )
 
     cluster_mgr = CloudHsmCluster(module)
