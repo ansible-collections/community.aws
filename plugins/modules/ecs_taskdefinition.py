8d221c9d47c48c342aced748b4e7253e82c55562
Vincent Tan <vtan@policyreporter.com>
2022-12-01 13:15:11 -0800

    Break up lines
    Signed-off-by: Vincent Tan <vtan@policyreporter.com>

--- plugins/modules/ecs_taskdefinition.py
+++ plugins/modules/ecs_taskdefinition.py
@@ -469,29 +469,40 @@ options:
                 suboptions:
                     command:
                         description:
-                            - A string array representing the command that the container runs to determine if it is healthy. The string array must start with CMD to run the command arguments directly, or CMD-SHELL to run the command with the container's default shell.
+                            - A string array representing the command that the container runs to determine if it is healthy.
+                            - The string array must start with CMD to run the command arguments directly,
+                            or CMD-SHELL to run the command with the container's default shell.
                             - An exit code of 0 indicates success, and non-zero exit code indicates failure.
                         required: False
                         type: list
                         elements: str
                     interval:
-                        description: The time period in seconds between each health check execution. You may specify between 5 and 300 seconds. The default value is 30 seconds.
+                        description:
+                            - The time period in seconds between each health check execution.
+                            - You may specify between 5 and 300 seconds. The default value is 30 seconds.
                         required: False
                         type: int
                         default: 30
                     retries:
-                        description: The number of times to retry a failed health check before the container is considered unhealthy. You may specify between 1 and 10 retries. The default value is 3.
+                        description:
+                            - The number of times to retry a failed health check before the container is considered unhealthy.
+                            - You may specify between 1 and 10 retries. The default value is 3.
                         required: False
                         type: int
                         default: 3
                     startPeriod:
                         description:
-                            - The optional grace period to provide containers time to bootstrap before failed health checks count towards the maximum number of retries. You can specify between 0 and 300 seconds. By default, the startPeriod is disabled.
-                            - Note: If a health check succeeds within the startPeriod, then the container is considered healthy and any subsequent failures count toward the maximum number of retries.
+                            - The optional grace period to provide containers time to bootstrap
+                            before failed health checks count towards the maximum number of retries.
+                            - You can specify between 0 and 300 seconds. By default, the startPeriod is disabled.
+                            - Note: If a health check succeeds within the startPeriod,
+                            then the container is considered healthy and any subsequent failures count toward the maximum number of retries.
                         required: False
                         type: int
                     timeout:
-                        description: The time period in seconds to wait for a health check to succeed before it is considered a failure. You may specify between 2 and 60 seconds. The default value is 5.
+                        description:
+                            - The time period in seconds to wait for a health check to succeed before it is considered a failure.
+                            - You may specify between 2 and 60 seconds. The default value is 5.
                         required: False
                         type: int
                         default: 5
