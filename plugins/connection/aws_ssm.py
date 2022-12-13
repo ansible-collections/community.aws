607e767172c6922410e667f7aeaefe7566027f92
Falk Lubiniecki <git-lubby@9f4.de>
2022-12-13 14:45:19 +0100

    add version added information
    Co-authored-by: Markus Bergholz <git@osuv.de>

--- plugins/connection/aws_ssm.py
+++ plugins/connection/aws_ssm.py
@@ -52,6 +52,7 @@ options:
     description: The endpoint URL of tht S3 bucket used for file transfers.
     vars:
     - name: ansible_aws_ssm_bucket_endpoint_url
+    version_added: 5.2.0
   plugin:
     description: This defines the location of the session-manager-plugin binary.
     vars:
