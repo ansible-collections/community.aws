9a5e3fc3438e5843e114ea03df401c856137d399
Justin McCormick <karcadia@gmail.com>
2023-01-05 09:04:20 -0600

    PEP8 Indentation Rules.

--- plugins/modules/ecs_cluster.py
+++ plugins/modules/ecs_cluster.py
@@ -255,10 +255,10 @@ def main():
         if existing and 'status' in existing and existing['status'] == "ACTIVE":
             if module.params['capacity_providers'] != existing['capacityProviders'] or \
                module.params['capacity_provider_strategy'] != existing['defaultCapacityProviderStrategy']:
-                 results = cluster_mgr.update_cluster(cluster_name=module.params['name'],
-                                                      capacity_providers=module.params['capacity_providers'],
-                                                      capacity_provider_strategy=module.params['capacity_provider_strategy'])
-                 results['changed'] = True
+                   results = cluster_mgr.update_cluster(cluster_name=module.params['name'],
+                                                        capacity_providers=module.params['capacity_providers'],
+                                                        capacity_provider_strategy=module.params['capacity_provider_strategy'])
+                   results['changed'] = True
             else:
               results['cluster'] = existing
         else:
