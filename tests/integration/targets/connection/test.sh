e01ac083262abc87f69d63d8ee7b2f93e06c3c43
Mark Chappell <mchappel@redhat.com>
2023-01-12 18:33:46 +0100

    aws_ssm - Rework (and enable) integration tests (#1652)
    aws_ssm - Rework (and enable) integration tests
    
    Depends-On: #558
    SUMMARY
    aws_ssm tests have been broken for a while.  This should get initial integration tests up and running
    ISSUE TYPE
    
    Feature Pull Request
    
    COMPONENT NAME
    aws_ssm
    ADDITIONAL INFORMATION
    Notes:
    
    Does not cover cross-geo S3 buckets (only one region in CI at the minute)
    Does not cover encrypted buckets (missing permissions)
    
    Reviewed-by: Markus Bergholz <git@osuv.de>
    Reviewed-by: Mark Chappell <None>

--- tests/integration/targets/connection/test.sh
+++ tests/integration/targets/connection/test.sh
@@ -4,7 +4,8 @@ set -eux
 
 [ -f "${INVENTORY}" ]
 
-# Run connection tests with both the default and C locale.
+ansible-playbook test_connection.yml -i "${INVENTORY}" "$@"
 
-                ansible-playbook test_connection.yml -i "${INVENTORY}" "$@"
-LC_ALL=C LANG=C ansible-playbook test_connection.yml -i "${INVENTORY}" "$@"
+# Ansible 2.14 dropped support for non UTF-8 Locale
+# https://github.com/ansible/ansible/pull/78175
+# LC_ALL=C LANG=C ansible-playbook test_connection.yml -i "${INVENTORY}" "$@"
