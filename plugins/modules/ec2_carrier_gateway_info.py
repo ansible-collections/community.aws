fea9e9ecf85b26cf1a6dfbace142cfb71ccfa63d
Mark Chappell <mchappel@redhat.com>
2023-03-30 09:47:54 +0200

    Update imports

--- plugins/modules/ec2_carrier_gateway_info.py
+++ plugins/modules/ec2_carrier_gateway_info.py
@@ -90,16 +90,18 @@ try:
 except ImportError:
     pass  # Handled by AnsibleAWSModule
 
-from ansible_collections.amazon.aws.plugins.module_utils.core import AnsibleAWSModule
-from ansible_collections.amazon.aws.plugins.module_utils.core import is_boto3_error_code
-from ansible_collections.amazon.aws.plugins.module_utils.ec2 import AWSRetry
-from ansible_collections.amazon.aws.plugins.module_utils.ec2 import boto3_tag_list_to_ansible_dict
-from ansible_collections.amazon.aws.plugins.module_utils.ec2 import ansible_dict_to_boto3_filter_list
-from ansible_collections.amazon.aws.plugins.module_utils.ec2 import camel_dict_to_snake_dict
+from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
+
+from ansible_collections.amazon.aws.plugins.module_utils.botocore import is_boto3_error_code
+from ansible_collections.amazon.aws.plugins.module_utils.retries import AWSRetry
+from ansible_collections.amazon.aws.plugins.module_utils.tagging import boto3_tag_list_to_ansible_dict
+from ansible_collections.amazon.aws.plugins.module_utils.transformation import ansible_dict_to_boto3_filter_list
+
+from ansible_collections.community.aws.plugins.module_utils.modules import AnsibleCommunityAWSModule as AnsibleAWSModule
 
 
 def get_carrier_gateway_info(carrier_gateway):
-    tags = carrier_gateway["Tags"]
+    tags = boto3_tag_list_to_ansible_dict(carrier_gateway["Tags"])
     ignore_list = []
     carrier_gateway_info = {
         "CarrierGatewayId": carrier_gateway["CarrierGatewayId"],
