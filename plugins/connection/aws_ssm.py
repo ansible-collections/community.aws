c207df31d7b5efb0c039a1153c8bcc3c54451677
richardsonky <52711804+richardsonky@users.noreply.github.com>
2023-04-11 06:58:37 -0500

    Remove ansi escape sequences

--- plugins/connection/aws_ssm.py
+++ plugins/connection/aws_ssm.py
@@ -742,7 +742,11 @@ class Connection(ConnectionBase):
             line = line.replace("\r\r\n", "\n")
             if len(line) == 201:
                 line = line[:-1]
-
+                
+        # Remove ansi escape sequences:
+        ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+        line = ansi_escape.sub('', line)
+        
         return line
 
     def _flush_stderr(self, session_process):
