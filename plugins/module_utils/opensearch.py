0be24580d2a4154ffb443987655075a7632b9209
Mark Chappell <mchappel@redhat.com>
2023-04-26 19:26:07 +0200

    Big Black PR (#1784)
    * Black prep
    
    * Black
    
    * changelog
    
    * Fix pylint unused-import in tests
    
    * Split SSM connection plugin changes
    
    * disable glue tests - bucket's missing
    
    * Disable s3_logging and s3_sync tests

--- plugins/module_utils/opensearch.py
+++ plugins/module_utils/opensearch.py
@@ -30,7 +30,10 @@ def get_domain_status(client, module, domain_name):
         response = client.describe_domain(DomainName=domain_name)
     except is_boto3_error_code("ResourceNotFoundException"):
         return None
-    except (botocore.exceptions.BotoCoreError, botocore.exceptions.ClientError) as e:  # pylint: disable=duplicate-except
+    except (
+        botocore.exceptions.BotoCoreError,
+        botocore.exceptions.ClientError,
+    ) as e:  # pylint: disable=duplicate-except
         module.fail_json_aws(e, msg="Couldn't get domain {0}".format(domain_name))
     return response["DomainStatus"]
 
@@ -49,7 +52,10 @@ def get_domain_config(client, module, domain_name):
         response = client.describe_domain_config(DomainName=domain_name)
     except is_boto3_error_code("ResourceNotFoundException"):
         return (None, None)
-    except (botocore.exceptions.BotoCoreError, botocore.exceptions.ClientError) as e:  # pylint: disable=duplicate-except
+    except (
+        botocore.exceptions.BotoCoreError,
+        botocore.exceptions.ClientError,
+    ) as e:  # pylint: disable=duplicate-except
         module.fail_json_aws(e, msg="Couldn't get domain {0}".format(domain_name))
     domain_config = {}
     arn = None
@@ -88,13 +94,9 @@ def normalize_opensearch(client, module, domain):
     convert the attributes from camel case to snake case, and return the object.
     """
     try:
-        domain["Tags"] = boto3_tag_list_to_ansible_dict(
-            client.list_tags(ARN=domain["ARN"], aws_retry=True)["TagList"]
-        )
+        domain["Tags"] = boto3_tag_list_to_ansible_dict(client.list_tags(ARN=domain["ARN"], aws_retry=True)["TagList"])
     except (botocore.exceptions.ClientError, botocore.exceptions.BotoCoreError) as e:
-        module.fail_json_aws(
-            e, "Couldn't get tags for domain %s" % domain["domain_name"]
-        )
+        module.fail_json_aws(e, "Couldn't get tags for domain %s" % domain["domain_name"])
     except KeyError:
         module.fail_json(msg=str(domain))
 
@@ -128,16 +130,14 @@ def wait_for_domain_status(client, module, domain_name, waiter_name):
                 return
         time.sleep(15)
     # Timeout occured.
-    module.fail_json(
-        msg=f"Timeout waiting for wait state '{waiter_name}'. {status_msg}"
-    )
+    module.fail_json(msg=f"Timeout waiting for wait state '{waiter_name}'. {status_msg}")
 
 
 def parse_version(engine_version):
-    '''
+    """
     Parse the engine version, which should be Elasticsearch_X.Y or OpenSearch_X.Y
     Return dict { 'engine_type': engine_type, 'major': major, 'minor': minor }
-    '''
+    """
     version = engine_version.split("_")
     if len(version) != 2:
         return None
@@ -145,19 +145,19 @@ def parse_version(engine_version):
     if len(semver) != 2:
         return None
     engine_type = version[0]
-    if engine_type not in ['Elasticsearch', 'OpenSearch']:
+    if engine_type not in ["Elasticsearch", "OpenSearch"]:
         return None
     if not (semver[0].isdigit() and semver[1].isdigit()):
         return None
     major = int(semver[0])
     minor = int(semver[1])
-    return {'engine_type': engine_type, 'major': major, 'minor': minor}
+    return {"engine_type": engine_type, "major": major, "minor": minor}
 
 
 def compare_domain_versions(version1, version2):
     supported_engines = {
-        'Elasticsearch': 1,
-        'OpenSearch': 2,
+        "Elasticsearch": 1,
+        "OpenSearch": 2,
     }
     if isinstance(version1, string_types):
         version1 = parse_version(version1)
@@ -169,21 +169,21 @@ def compare_domain_versions(version1, version2):
         return 1
     elif version1 is None and version2 is None:
         return 0
-    e1 = supported_engines.get(version1.get('engine_type'))
-    e2 = supported_engines.get(version2.get('engine_type'))
+    e1 = supported_engines.get(version1.get("engine_type"))
+    e2 = supported_engines.get(version2.get("engine_type"))
     if e1 < e2:
         return -1
     elif e1 > e2:
         return 1
     else:
-        if version1.get('major') < version2.get('major'):
+        if version1.get("major") < version2.get("major"):
             return -1
-        elif version1.get('major') > version2.get('major'):
+        elif version1.get("major") > version2.get("major"):
             return 1
         else:
-            if version1.get('minor') < version2.get('minor'):
+            if version1.get("minor") < version2.get("minor"):
                 return -1
-            elif version1.get('minor') > version2.get('minor'):
+            elif version1.get("minor") > version2.get("minor"):
                 return 1
             else:
                 return 0
@@ -203,22 +203,15 @@ def get_target_increment_version(client, module, domain_name, target_version):
     except (botocore.exceptions.BotoCoreError, botocore.exceptions.ClientError) as e:
         module.fail_json_aws(
             e,
-            msg="Couldn't get compatible versions for domain {0}".format(
-                domain_name),
+            msg="Couldn't get compatible versions for domain {0}".format(domain_name),
         )
-    compat = api_compatible_versions.get('CompatibleVersions')
+    compat = api_compatible_versions.get("CompatibleVersions")
     if compat is None:
-        module.fail_json(
-            "Unable to determine list of compatible versions",
-            compatible_versions=api_compatible_versions)
+        module.fail_json("Unable to determine list of compatible versions", compatible_versions=api_compatible_versions)
     if len(compat) == 0:
-        module.fail_json(
-            "Unable to determine list of compatible versions",
-            compatible_versions=api_compatible_versions)
+        module.fail_json("Unable to determine list of compatible versions", compatible_versions=api_compatible_versions)
     if compat[0].get("TargetVersions") is None:
-        module.fail_json(
-            "No compatible versions found",
-            compatible_versions=api_compatible_versions)
+        module.fail_json("No compatible versions found", compatible_versions=api_compatible_versions)
     compatible_versions = []
     for v in compat[0].get("TargetVersions"):
         if target_version == v:
@@ -243,9 +236,7 @@ def ensure_tags(client, module, resource_arn, existing_tags, tags, purge_tags):
     changed = bool(tags_to_add or tags_to_remove)
     if tags_to_add:
         if module.check_mode:
-            module.exit_json(
-                changed=True, msg="Would have added tags to domain if not in check mode"
-            )
+            module.exit_json(changed=True, msg="Would have added tags to domain if not in check mode")
         try:
             client.add_tags(
                 ARN=resource_arn,
@@ -255,21 +246,15 @@ def ensure_tags(client, module, resource_arn, existing_tags, tags, purge_tags):
             botocore.exceptions.ClientError,
             botocore.exceptions.BotoCoreError,
         ) as e:
-            module.fail_json_aws(
-                e, "Couldn't add tags to domain {0}".format(resource_arn)
-            )
+            module.fail_json_aws(e, "Couldn't add tags to domain {0}".format(resource_arn))
     if tags_to_remove:
         if module.check_mode:
-            module.exit_json(
-                changed=True, msg="Would have removed tags if not in check mode"
-            )
+            module.exit_json(changed=True, msg="Would have removed tags if not in check mode")
         try:
             client.remove_tags(ARN=resource_arn, TagKeys=tags_to_remove)
         except (
             botocore.exceptions.ClientError,
             botocore.exceptions.BotoCoreError,
         ) as e:
-            module.fail_json_aws(
-                e, "Couldn't remove tags from domain {0}".format(resource_arn)
-            )
+            module.fail_json_aws(e, "Couldn't remove tags from domain {0}".format(resource_arn))
     return changed
--- tests/unit/plugins/modules/test_directconnect_virtual_interface.py
+++ tests/unit/plugins/modules/test_directconnect_virtual_interface.py
@@ -4,20 +4,27 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 # Make coding more python3-ish
-from __future__ import (absolute_import, division, print_function)
+from __future__ import absolute_import, division, print_function
+
 __metaclass__ = type
 
 import pytest
 
 from ansible_collections.amazon.aws.plugins.module_utils.botocore import HAS_BOTO3
+
 # Magic...  Incorrectly identified by pylint as unused
-from ansible_collections.amazon.aws.tests.unit.utils.amazon_placebo_fixtures import maybe_sleep  # pylint: disable=unused-import
-from ansible_collections.amazon.aws.tests.unit.utils.amazon_placebo_fixtures import placeboify  # pylint: disable=unused-import
+# pylint: disable-next=unused-import
+from ansible_collections.amazon.aws.tests.unit.utils.amazon_placebo_fixtures import maybe_sleep
+
+# pylint: disable-next=unused-import
+from ansible_collections.amazon.aws.tests.unit.utils.amazon_placebo_fixtures import placeboify
 
 from ansible_collections.community.aws.plugins.modules import directconnect_virtual_interface
 
 if not HAS_BOTO3:
-    pytestmark = pytest.mark.skip("test_directconnect_confirm_connection.py requires the `boto3` and `botocore` modules")
+    pytestmark = pytest.mark.skip(
+        "test_directconnect_confirm_connection.py requires the `boto3` and `botocore` modules"
+    )
 
 
 class FailException(Exception):
@@ -46,10 +53,7 @@ def test_find_unique_vi_by_connection_id(placeboify, maybe_sleep):
 
 def test_find_unique_vi_by_vi_id(placeboify, maybe_sleep):
     client = placeboify.client("directconnect")
-    vi_id = directconnect_virtual_interface.find_unique_vi(client,
-                                                           None,
-                                                           "dxvif-aaaaaaaaa",
-                                                           None)
+    vi_id = directconnect_virtual_interface.find_unique_vi(client, None, "dxvif-aaaaaaaaa", None)
     assert vi_id == "dxvif-aaaaaaaa"
 
 
@@ -61,47 +65,38 @@ def test_find_unique_vi_by_name(placeboify, maybe_sleep):
 
 def test_find_unique_vi_returns_multiple(placeboify, maybe_sleep):
     client = placeboify.client("directconnect")
-    module = FakeModule(state="present",
-                        id_to_associate="dxcon-aaaaaaaa",
-                        public=False,
-                        name=None)
+    module = FakeModule(state="present", id_to_associate="dxcon-aaaaaaaa", public=False, name=None)
     with pytest.raises(FailException):
-        directconnect_virtual_interface.ensure_state(
-            client,
-            module
-        )
+        directconnect_virtual_interface.ensure_state(client, module)
     assert "Multiple virtual interfaces were found" in module.exit_kwargs["msg"]
 
 
 def test_find_unique_vi_returns_missing_for_vi_id(placeboify, maybe_sleep):
     client = placeboify.client("directconnect")
-    module = FakeModule(state="present",
-                        id_to_associate=None,
-                        public=False,
-                        name=None,
-                        virtual_interface_id="dxvif-aaaaaaaa")
+    module = FakeModule(
+        state="present", id_to_associate=None, public=False, name=None, virtual_interface_id="dxvif-aaaaaaaa"
+    )
     with pytest.raises(FailException):
-        directconnect_virtual_interface.ensure_state(
-            client,
-            module
-        )
+        directconnect_virtual_interface.ensure_state(client, module)
     assert "The virtual interface dxvif-aaaaaaaa does not exist" in module.exit_kwargs["msg"]
 
 
 def test_construct_public_vi():
-    module = FakeModule(state="present",
-                        id_to_associate=None,
-                        public=True,
-                        name="aaaaaaaa",
-                        vlan=1,
-                        bgp_asn=123,
-                        authentication_key="aaaa",
-                        customer_address="169.254.0.1/30",
-                        amazon_address="169.254.0.2/30",
-                        address_type="ipv4",
-                        cidr=["10.88.0.0/30"],
-                        virtual_gateway_id="xxxx",
-                        direct_connect_gateway_id="yyyy")
+    module = FakeModule(
+        state="present",
+        id_to_associate=None,
+        public=True,
+        name="aaaaaaaa",
+        vlan=1,
+        bgp_asn=123,
+        authentication_key="aaaa",
+        customer_address="169.254.0.1/30",
+        amazon_address="169.254.0.2/30",
+        address_type="ipv4",
+        cidr=["10.88.0.0/30"],
+        virtual_gateway_id="xxxx",
+        direct_connect_gateway_id="yyyy",
+    )
     vi = directconnect_virtual_interface.assemble_params_for_creating_vi(module.params)
     assert vi == {
         "virtualInterfaceName": "aaaaaaaa",
@@ -111,24 +106,26 @@ def test_construct_public_vi():
         "amazonAddress": "169.254.0.2/30",
         "customerAddress": "169.254.0.1/30",
         "addressFamily": "ipv4",
-        "routeFilterPrefixes": [{"cidr": "10.88.0.0/30"}]
+        "routeFilterPrefixes": [{"cidr": "10.88.0.0/30"}],
     }
 
 
 def test_construct_private_vi_with_virtual_gateway_id():
-    module = FakeModule(state="present",
-                        id_to_associate=None,
-                        public=False,
-                        name="aaaaaaaa",
-                        vlan=1,
-                        bgp_asn=123,
-                        authentication_key="aaaa",
-                        customer_address="169.254.0.1/30",
-                        amazon_address="169.254.0.2/30",
-                        address_type="ipv4",
-                        cidr=["10.88.0.0/30"],
-                        virtual_gateway_id="xxxx",
-                        direct_connect_gateway_id="yyyy")
+    module = FakeModule(
+        state="present",
+        id_to_associate=None,
+        public=False,
+        name="aaaaaaaa",
+        vlan=1,
+        bgp_asn=123,
+        authentication_key="aaaa",
+        customer_address="169.254.0.1/30",
+        amazon_address="169.254.0.2/30",
+        address_type="ipv4",
+        cidr=["10.88.0.0/30"],
+        virtual_gateway_id="xxxx",
+        direct_connect_gateway_id="yyyy",
+    )
     vi = directconnect_virtual_interface.assemble_params_for_creating_vi(module.params)
     assert vi == {
         "virtualInterfaceName": "aaaaaaaa",
@@ -138,24 +135,26 @@ def test_construct_private_vi_with_virtual_gateway_id():
         "amazonAddress": "169.254.0.2/30",
         "customerAddress": "169.254.0.1/30",
         "addressFamily": "ipv4",
-        "virtualGatewayId": "xxxx"
+        "virtualGatewayId": "xxxx",
     }
 
 
 def test_construct_private_vi_with_direct_connect_gateway_id():
-    module = FakeModule(state="present",
-                        id_to_associate=None,
-                        public=False,
-                        name="aaaaaaaa",
-                        vlan=1,
-                        bgp_asn=123,
-                        authentication_key="aaaa",
-                        customer_address="169.254.0.1/30",
-                        amazon_address="169.254.0.2/30",
-                        address_type="ipv4",
-                        cidr=["10.88.0.0/30"],
-                        virtual_gateway_id=None,
-                        direct_connect_gateway_id="yyyy")
+    module = FakeModule(
+        state="present",
+        id_to_associate=None,
+        public=False,
+        name="aaaaaaaa",
+        vlan=1,
+        bgp_asn=123,
+        authentication_key="aaaa",
+        customer_address="169.254.0.1/30",
+        amazon_address="169.254.0.2/30",
+        address_type="ipv4",
+        cidr=["10.88.0.0/30"],
+        virtual_gateway_id=None,
+        direct_connect_gateway_id="yyyy",
+    )
     vi = directconnect_virtual_interface.assemble_params_for_creating_vi(module.params)
     print(vi)
     assert vi == {
@@ -166,26 +165,28 @@ def test_construct_private_vi_with_direct_connect_gateway_id():
         "amazonAddress": "169.254.0.2/30",
         "customerAddress": "169.254.0.1/30",
         "addressFamily": "ipv4",
-        "directConnectGatewayId": "yyyy"
+        "directConnectGatewayId": "yyyy",
     }
 
 
 def test_create_public_vi(placeboify, maybe_sleep):
     client = placeboify.client("directconnect")
-    module = FakeModule(state="present",
-                        id_to_associate='dxcon-aaaaaaaa',
-                        virtual_interface_id=None,
-                        public=True,
-                        name="aaaaaaaa",
-                        vlan=1,
-                        bgp_asn=123,
-                        authentication_key="aaaa",
-                        customer_address="169.254.0.1/30",
-                        amazon_address="169.254.0.2/30",
-                        address_type="ipv4",
-                        cidr=["10.88.0.0/30"],
-                        virtual_gateway_id="xxxx",
-                        direct_connect_gateway_id="yyyy")
+    module = FakeModule(
+        state="present",
+        id_to_associate="dxcon-aaaaaaaa",
+        virtual_interface_id=None,
+        public=True,
+        name="aaaaaaaa",
+        vlan=1,
+        bgp_asn=123,
+        authentication_key="aaaa",
+        customer_address="169.254.0.1/30",
+        amazon_address="169.254.0.2/30",
+        address_type="ipv4",
+        cidr=["10.88.0.0/30"],
+        virtual_gateway_id="xxxx",
+        direct_connect_gateway_id="yyyy",
+    )
     changed, latest_state = directconnect_virtual_interface.ensure_state(client, module)
     assert changed is True
     assert latest_state is not None
@@ -193,20 +194,22 @@ def test_create_public_vi(placeboify, maybe_sleep):
 
 def test_create_private_vi(placeboify, maybe_sleep):
     client = placeboify.client("directconnect")
-    module = FakeModule(state="present",
-                        id_to_associate='dxcon-aaaaaaaa',
-                        virtual_interface_id=None,
-                        public=False,
-                        name="aaaaaaaa",
-                        vlan=1,
-                        bgp_asn=123,
-                        authentication_key="aaaa",
-                        customer_address="169.254.0.1/30",
-                        amazon_address="169.254.0.2/30",
-                        address_type="ipv4",
-                        cidr=["10.88.0.0/30"],
-                        virtual_gateway_id="xxxx",
-                        direct_connect_gateway_id="yyyy")
+    module = FakeModule(
+        state="present",
+        id_to_associate="dxcon-aaaaaaaa",
+        virtual_interface_id=None,
+        public=False,
+        name="aaaaaaaa",
+        vlan=1,
+        bgp_asn=123,
+        authentication_key="aaaa",
+        customer_address="169.254.0.1/30",
+        amazon_address="169.254.0.2/30",
+        address_type="ipv4",
+        cidr=["10.88.0.0/30"],
+        virtual_gateway_id="xxxx",
+        direct_connect_gateway_id="yyyy",
+    )
     changed, latest_state = directconnect_virtual_interface.ensure_state(client, module)
     assert changed is True
     assert latest_state is not None
@@ -214,20 +217,22 @@ def test_create_private_vi(placeboify, maybe_sleep):
 
 def test_delete_vi(placeboify, maybe_sleep):
     client = placeboify.client("directconnect")
-    module = FakeModule(state="absent",
-                        id_to_associate='dxcon-aaaaaaaa',
-                        virtual_interface_id='dxvif-aaaaaaaa',
-                        public=False,
-                        name="aaaaaaaa",
-                        vlan=1,
-                        bgp_asn=123,
-                        authentication_key="aaaa",
-                        customer_address="169.254.0.1/30",
-                        amazon_address="169.254.0.2/30",
-                        address_type="ipv4",
-                        cidr=["10.88.0.0/30"],
-                        virtual_gateway_id=None,
-                        direct_connect_gateway_id="yyyy")
+    module = FakeModule(
+        state="absent",
+        id_to_associate="dxcon-aaaaaaaa",
+        virtual_interface_id="dxvif-aaaaaaaa",
+        public=False,
+        name="aaaaaaaa",
+        vlan=1,
+        bgp_asn=123,
+        authentication_key="aaaa",
+        customer_address="169.254.0.1/30",
+        amazon_address="169.254.0.2/30",
+        address_type="ipv4",
+        cidr=["10.88.0.0/30"],
+        virtual_gateway_id=None,
+        direct_connect_gateway_id="yyyy",
+    )
     changed, latest_state = directconnect_virtual_interface.ensure_state(client, module)
     assert changed is True
     assert latest_state == {}
