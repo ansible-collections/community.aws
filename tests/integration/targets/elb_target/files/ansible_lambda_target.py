2c4575c248776c65d66b06cd60fa09b0dae1cd6f
Mark Chappell <mchappel@redhat.com>
2023-04-26 19:26:07 +0200

    Big Black PR (#1784)
    * Black prep
    
    * Black
    
    * changelog
    
    * Fix pylint unused-import in tests
    
    * Split SSM connection plugin changes
    
    * disable glue tests - bucket's missing
    
    * Disable s3_logging and s3_sync tests

--- tests/integration/targets/elb_target/files/ansible_lambda_target.py
+++ tests/integration/targets/elb_target/files/ansible_lambda_target.py
@@ -1,10 +1,8 @@
-from __future__ import (absolute_import, division, print_function)
+from __future__ import absolute_import, division, print_function
+
 __metaclass__ = type
 import json
 
 
 def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
