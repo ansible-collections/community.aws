8ab2797829549625ffd4f761570e21c99dcb9792
Mark Chappell <mchappel@redhat.com>
2023-10-17 13:44:40 +0200

    Skip changes to aws_ssm plugin for now, unstable tests

--- plugins/connection/aws_ssm.py
+++ plugins/connection/aws_ssm.py
@@ -262,9 +262,9 @@ EXAMPLES = r"""
         state: present
 """
 
+import os
 import getpass
 import json
-import os
 import pty
 import random
 import re
@@ -280,19 +280,17 @@ except ImportError as e:
     pass
 
 from functools import wraps
-
+from ansible_collections.amazon.aws.plugins.module_utils.botocore import HAS_BOTO3
 from ansible.errors import AnsibleConnectionFailure
 from ansible.errors import AnsibleError
 from ansible.errors import AnsibleFileNotFound
-from ansible.module_utils._text import to_bytes
-from ansible.module_utils._text import to_text
 from ansible.module_utils.basic import missing_required_lib
 from ansible.module_utils.six.moves import xrange
+from ansible.module_utils._text import to_bytes
+from ansible.module_utils._text import to_text
 from ansible.plugins.connection import ConnectionBase
 from ansible.plugins.shell.powershell import _common_args
 from ansible.utils.display import Display
-
-from ansible_collections.amazon.aws.plugins.module_utils.botocore import HAS_BOTO3
 
 display = Display()
 
