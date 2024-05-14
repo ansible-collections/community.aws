6aab22bfea8b62aaeb75125c9616979468923fbc
alicekaerast <alice_kaerast@flutterint.com>
2024-05-14 07:40:55 +0100

    Document IAM actions required

--- plugins/modules/acm_certificate_info.py
+++ plugins/modules/acm_certificate_info.py
@@ -13,6 +13,7 @@ description:
   - Note that this will not return information about uploaded keys of size 4096 bits, due to a limitation of the ACM API.
   - Prior to release 5.0.0 this module was called C(community.aws.aws_acm_info).
     The usage did not change.
+  - Requires the IAM permissions 'acm:ListCertificates', 'acm:DescribeCertificate', 'acm:GetCertificate', 'acm:ListTagsForCertificate'
 options:
   certificate_arn:
     description:
