259624c3ba1bc705154697e1dba107de9d149a44
Eli Acherkan <eacherkan@riverbed.com>
2024-07-19 15:24:12 +0300

    ecs_taskdefinition: `aws_retry=True` on all API calls

--- plugins/modules/ecs_taskdefinition.py
+++ plugins/modules/ecs_taskdefinition.py
@@ -916,7 +916,7 @@ class EcsTaskManager:
             if data["nextToken"]:
                 params["nextToken"] = data["nextToken"]
 
-            result = self.ecs.list_task_definitions(**params)
+            result = self.ecs.list_task_definitions(aws_retry=True, **params)
             data["taskDefinitionArns"] += result["taskDefinitionArns"]
             data["nextToken"] = result.get("nextToken", None)
             return data["nextToken"] is not None
@@ -937,7 +937,7 @@ class EcsTaskManager:
         )
 
     def deregister_task(self, taskArn):
-        response = self.ecs.deregister_task_definition(taskDefinition=taskArn)
+        response = self.ecs.deregister_task_definition(aws_retry=True, taskDefinition=taskArn)
         return response["taskDefinition"]
 
 
