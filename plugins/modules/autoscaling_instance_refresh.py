4409682899c4727c32e5fe3dea14861fa6ae0cbb
abikouo <abikouo@redhat.com>
2024-09-20 16:13:35 +0200

    Refactor autoscaling_instance_refresh and autoscaling_instance_refresh_info modules

--- plugins/modules/autoscaling_instance_refresh.py
+++ plugins/modules/autoscaling_instance_refresh.py
@@ -87,95 +87,93 @@ EXAMPLES = r"""
 """
 
 RETURN = r"""
----
-instance_refresh_id:
-    description: instance refresh id
-    returned: success
-    type: str
-    sample: "08b91cf7-8fa6-48af-b6a6-d227f40f1b9b"
-auto_scaling_group_name:
-    description: Name of autoscaling group
-    returned: success
-    type: str
-    sample: "public-webapp-production-1"
-status:
-    description:
-      -  The current state of the group when DeleteAutoScalingGroup is in progress.
-      -  The following are the possible statuses
-      -    Pending --  The request was created, but the operation has not started.
-      -    InProgress --  The operation is in progress.
-      -    Successful --  The operation completed successfully.
-      -    Failed --  The operation failed to complete. You can troubleshoot using the status reason and the scaling activities.
-      -    Cancelling --
-      -        An ongoing operation is being cancelled.
-      -        Cancellation does not roll back any replacements that have already been completed,
-      -        but it prevents new replacements from being started.
-      -    Cancelled --  The operation is cancelled.
-    returned: success
-    type: str
-    sample: "Pending"
-start_time:
-    description: The date and time this ASG was created, in ISO 8601 format.
-    returned: success
-    type: str
-    sample: "2015-11-25T00:05:36.309Z"
-end_time:
-    description: The date and time this ASG was created, in ISO 8601 format.
-    returned: success
-    type: str
-    sample: "2015-11-25T00:05:36.309Z"
-percentage_complete:
-    description: the % of completeness
-    returned: success
-    type: int
-    sample: 100
-instances_to_update:
-    description: num. of instance to update
-    returned: success
-    type: int
-    sample: 5
+instance_refreshes:
+    description: Details of the instance refreshes for the Auto Scaling group..
+    returned: always
+    type: complex
+    contains:
+        instance_refresh_id:
+            description: instance refresh id
+            returned: success
+            type: str
+            sample: "08b91cf7-8fa6-48af-b6a6-d227f40f1b9b"
+        auto_scaling_group_name:
+            description: Name of autoscaling group
+            returned: success
+            type: str
+            sample: "public-webapp-production-1"
+        status:
+            description:
+              - The current state of the group when DeleteAutoScalingGroup is in progress.
+              - The following are the possible statuses
+              - Pending - The request was created, but the operation has not started.
+              - InProgress - The operation is in progress.
+              - Successful - The operation completed successfully.
+              - Failed - The operation failed to complete.
+                You can troubleshoot using the status reason and the scaling activities.
+              - Cancelling - An ongoing operation is being cancelled.
+                Cancellation does not roll back any replacements that have already been
+                completed, but it prevents new replacements from being started.
+              - Cancelled - The operation is cancelled.'
+            returned: success
+            type: str
+            sample: "Pending"
+        preferences:
+            description: The preferences for an instance refresh.
+            returned: always
+            type: dict
+            sample: {
+                'AlarmSpecification': {
+                    'Alarms': [
+                        'my-alarm',
+                    ],
+                },
+                'AutoRollback': True,
+                'InstanceWarmup': 200,
+                'MinHealthyPercentage': 90,
+                'ScaleInProtectedInstances': 'Ignore',
+                'SkipMatching': False,
+                'StandbyInstances': 'Ignore',
+            }
+        start_time:
+            description: The date and time this ASG was created, in ISO 8601 format.
+            returned: success
+            type: str
+            sample: "2015-11-25T00:05:36.309Z"
+        end_time:
+            description: The date and time this ASG was created, in ISO 8601 format.
+            returned: success
+            type: str
+            sample: "2015-11-25T00:05:36.309Z"
+        percentage_complete:
+            description: the % of completeness
+            returned: success
+            type: int
+            sample: 100
+        instances_to_update:
+            description: num. of instance to update
+            returned: success
+            type: int
+            sample: 5
 """
-
-try:
-    from botocore.exceptions import BotoCoreError
-    from botocore.exceptions import ClientError
-except ImportError:
-    pass  # caught by AnsibleAWSModule
 
 from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
 from ansible.module_utils.common.dict_transformations import snake_dict_to_camel_dict
 
-from ansible_collections.amazon.aws.plugins.module_utils.retries import AWSRetry
+from ansible_collections.amazon.aws.plugins.module_utils.autoscaling import AnsibleAutoScalingError
+from ansible_collections.amazon.aws.plugins.module_utils.autoscaling import cancel_instance_refresh
+from ansible_collections.amazon.aws.plugins.module_utils.autoscaling import describe_instance_refreshes
+from ansible_collections.amazon.aws.plugins.module_utils.autoscaling import start_instance_refresh
 from ansible_collections.amazon.aws.plugins.module_utils.transformation import scrub_none_parameters
 
 from ansible_collections.community.aws.plugins.module_utils.modules import AnsibleCommunityAWSModule as AnsibleAWSModule
 
 
-def start_or_cancel_instance_refresh(conn, module):
+def start_or_cancel_instance_refresh(conn, module: AnsibleAWSModule) -> None:
     """
     Args:
         conn (boto3.AutoScaling.Client): Valid Boto3 ASG client.
         module: AnsibleAWSModule object
-
-    Returns:
-        {
-            "instance_refreshes": [
-                    {
-                        'auto_scaling_group_name': 'ansible-test-hermes-63642726-asg',
-                        'instance_refresh_id': '6507a3e5-4950-4503-8978-e9f2636efc09',
-                        'instances_to_update': 1,
-                        'percentage_complete': 0,
-                        "preferences": {
-                            "instance_warmup": 60,
-                            "min_healthy_percentage": 90,
-                            "skip_matching": false
-                        },
-                        'start_time': '2021-02-04T03:39:40+00:00',
-                        'status': 'Cancelling',
-                        'status_reason': 'Replacing instances before cancelling.',
-                    }
-              ]
-        }
     """
 
     asg_state = module.params.get("state")
@@ -183,24 +181,22 @@ def start_or_cancel_instance_refresh(conn, module):
     preferences = module.params.get("preferences")
 
     args = {}
-    args["AutoScalingGroupName"] = asg_name
     if asg_state == "started":
         args["Strategy"] = module.params.get("strategy")
     if preferences:
         if asg_state == "cancelled":
             module.fail_json(msg="can not pass preferences dict when canceling a refresh")
-        _prefs = scrub_none_parameters(preferences)
-        args["Preferences"] = snake_dict_to_camel_dict(_prefs, capitalize_first=True)
+        args["Preferences"] = snake_dict_to_camel_dict(scrub_none_parameters(preferences), capitalize_first=True)
     cmd_invocations = {
-        "cancelled": conn.cancel_instance_refresh,
-        "started": conn.start_instance_refresh,
+        "cancelled": cancel_instance_refresh,
+        "started": start_instance_refresh,
     }
     try:
         if module.check_mode:
+            ongoing_refresh = describe_instance_refreshes(conn, auto_scaling_group_name=asg_name).get(
+                "InstanceRefreshes", []
+            )
             if asg_state == "started":
-                ongoing_refresh = conn.describe_instance_refreshes(AutoScalingGroupName=asg_name).get(
-                    "InstanceRefreshes", "[]"
-                )
                 if ongoing_refresh:
                     module.exit_json(
                         changed=False,
@@ -209,26 +205,23 @@ def start_or_cancel_instance_refresh(conn, module):
                 else:
                     module.exit_json(changed=True, msg="Would have started instance refresh if not in check mode.")
             elif asg_state == "cancelled":
-                ongoing_refresh = conn.describe_instance_refreshes(AutoScalingGroupName=asg_name).get(
-                    "InstanceRefreshes", "[]"
-                )[0]
-                if ongoing_refresh.get("Status", "") in ["Cancelling", "Cancelled"]:
+                if ongoing_refresh and ongoing_refresh[0].get("Status", "") in ["Cancelling", "Cancelled"]:
                     module.exit_json(
                         changed=False,
                         msg="In check_mode - Instance Refresh already cancelled or is pending cancellation.",
                     )
                 elif not ongoing_refresh:
-                    module.exit_json(chaned=False, msg="In check_mode - No active referesh found, nothing to cancel.")
+                    module.exit_json(changed=False, msg="In check_mode - No active referesh found, nothing to cancel.")
                 else:
                     module.exit_json(changed=True, msg="Would have cancelled instance refresh if not in check mode.")
-        result = cmd_invocations[asg_state](aws_retry=True, **args)
-        instance_refreshes = conn.describe_instance_refreshes(
-            AutoScalingGroupName=asg_name, InstanceRefreshIds=[result["InstanceRefreshId"]]
+        instance_refresh_id = cmd_invocations[asg_state](conn, auto_scaling_group_name=asg_name, **args)
+        response = describe_instance_refreshes(
+            conn, auto_scaling_group_name=asg_name, instance_refresh_ids=[instance_refresh_id]
         )
-        result = dict(instance_refreshes=camel_dict_to_snake_dict(instance_refreshes["InstanceRefreshes"][0]))
-        return module.exit_json(**result)
-    except (BotoCoreError, ClientError) as e:
-        module.fail_json_aws(e, msg=f"Failed to {asg_state.replace('ed', '')} InstanceRefresh")
+        result = dict(instance_refreshes=camel_dict_to_snake_dict(response["InstanceRefreshes"][0]))
+        module.exit_json(**result)
+    except AnsibleAutoScalingError as e:
+        module.fail_json_aws(e, msg=f"Failed to {asg_state.replace('ed', '')} InstanceRefresh: {e}")
 
 
 def main():
@@ -254,10 +247,7 @@ def main():
         argument_spec=argument_spec,
         supports_check_mode=True,
     )
-    autoscaling = module.client(
-        "autoscaling",
-        retry_decorator=AWSRetry.jittered_backoff(retries=10, catch_extra_error_codes=["InstanceRefreshInProgress"]),
-    )
+    autoscaling = module.client("autoscaling")
 
     start_or_cancel_instance_refresh(autoscaling, module)
 
