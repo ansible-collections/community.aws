f89457a8ff14e59a498d55cdb15d1695b45d5d90
Mandar Kulkarni <mandar242@gmail.com>
2024-10-02 11:13:34 -0700

    use from ansible_collections.amazon.aws.plugins.module_utils.ec2 import AnsibleEC2Error

--- plugins/modules/ec2_transit_gateway_info.py
+++ plugins/modules/ec2_transit_gateway_info.py
@@ -171,6 +171,7 @@ from typing import Dict
 from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
 
 from ansible_collections.amazon.aws.plugins.module_utils.botocore import is_boto3_error_code
+from ansible_collections.amazon.aws.plugins.module_utils.ec2 import AnsibleEC2Error
 from ansible_collections.amazon.aws.plugins.module_utils.retries import AWSRetry
 from ansible_collections.amazon.aws.plugins.module_utils.tagging import boto3_tag_list_to_ansible_dict
 from ansible_collections.amazon.aws.plugins.module_utils.transformation import ansible_dict_to_boto3_filter_list
@@ -245,7 +246,7 @@ def main():
     client = AnsibleEc2TgwInfo(module=module, results=results)
     try:
         client.describe_transit_gateways()
-    except (botocore.exceptions.ClientError, botocore.exceptions.BotoCoreError) as e:
+    except AnsibleEC2Error as e:
         module.fail_json_aws(e)
 
     module.exit_json(**results)
