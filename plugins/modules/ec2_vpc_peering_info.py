2b811a9eec769cdb14924fdc614f7a15ab40a689
Alina Buzachis <abuzachis@redhat.com>
2024-10-10 13:29:49 +0200

    Update ec2_vpc_peering_info.py

--- plugins/modules/ec2_vpc_peering_info.py
+++ plugins/modules/ec2_vpc_peering_info.py
@@ -342,7 +342,6 @@ from typing import List
 from ansible.module_utils.common.dict_transformations import camel_dict_to_snake_dict
 
 from ansible_collections.amazon.aws.plugins.module_utils.botocore import normalize_boto3_result
-from ansible_collections.amazon.aws.plugins.module_utils.ec2 import AnsibleEC2Error
 from ansible_collections.amazon.aws.plugins.module_utils.ec2 import describe_vpc_peering_connections
 from ansible_collections.amazon.aws.plugins.module_utils.tagging import boto3_tag_list_to_ansible_dict
 from ansible_collections.amazon.aws.plugins.module_utils.transformation import ansible_dict_to_boto3_filter_list
