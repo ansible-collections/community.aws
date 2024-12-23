25258af7b16bf4aef1a05648373e9d4a5b51262e
JavidMinadathAlimohi <javidalimohideenmeerasa@fico.com>
2024-12-23 15:22:20 -0500

    1. Include unit test for function "filters_are_equal" 2. Included a changelog minor change fragment file. 3. Enclosed types within double quotes.

---
+++ tests/unit/plugins/modules/test_s3_lifecycle_filters_are_equal.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2023, Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+import pytest
+
+from ansible_collections.community.aws.plugins.modules.s3_lifecycle import filters_are_equal
+
+def test_filters_are_equal():
+    # Test case 1: Both filters are identical
+    filter1 = {
+        "And": {
+            "Prefix": "nested/",
+            "ObjectSizeGreaterThan": 150,
+            "ObjectSizeLessThan": 180,
+        },
+    }
+    filter2 = filter1.copy()
+    assert filters_are_equal(filter1, filter2) is True
+
+    # Test case 2: One filter is None
+    filter1 = None
+    filter2 = {
+        "Prefix": "test/"
+    }
+    assert filters_are_equal(filter1, filter2) is False
+
+    # Test case 3: One filter is empty
+    filter1 = {}
+    filter2 = {
+        "ObjectSizeGreaterThan": 100,
+    }
+    assert filters_are_equal(filter1, filter2) is False
+
+    # Test case 4: Filters differ in a single key
+    filter1 = {
+        "ObjectSizeGreaterThan": 100,
+    }
+    filter2 = {
+        "ObjectSizeGreaterThan": 200,  # Different value
+    }
+    assert filters_are_equal(filter1, filter2) is False
+
+    # Test case 5: Filters with missing `And` key in one filter
+    filter1 = {
+        "Prefix": "test/",
+    }
+    filter2 = {
+        "And": {
+            "Prefix": "nested/",
+            "ObjectSizeGreaterThan": 100,
+        },
+    }
+    assert filters_are_equal(filter1, filter2) is False
+
+    # Test case 6: Filters with nested `And` keys matching
+    filter1 = {
+        "And": {
+            "Prefix": "nested/",
+            "ObjectSizeGreaterThan": 150,
+        },
+    }
+    filter2 = {
+        "And": {
+            "Prefix": "nested/",
+            "ObjectSizeGreaterThan": 150,
+        },
+    }
+    assert filters_are_equal(filter1, filter2) is True
+
+    # Test case 7: Filters with nested `And` keys differing
+    filter1 = {
+        "And": {
+            "Prefix": "test/",
+            "ObjectSizeGreaterThan": 150,
+        },
+    }
+    filter2 = {
+        "And": {
+            "Prefix": "nested/", # Different key/value
+            "ObjectSizeLessThan": 150,  
+        },
+    }
+    assert filters_are_equal(filter1, filter2) is False
+
+    # Test case 8: Both filters are None
+    filter1 = None
+    filter2 = None
+    assert filters_are_equal(filter1, filter2) is False
+
+    # Test case 9: Filters with different `Prefix` values
+    filter1 = {
+        "Prefix": "test/"
+    }
+    filter2 = {
+        "Prefix": "different/"
+    }
+    assert filters_are_equal(filter1, filter2) is False
+
+    # Test case 10: Filters with empty strings for `Prefix`
+    filter1 = {
+        "Prefix": ""
+    }
+    filter2 = {
+        "Prefix": ""
+    }
+    assert filters_are_equal(filter1, filter2) is True
