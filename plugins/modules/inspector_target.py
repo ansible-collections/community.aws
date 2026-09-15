627ce91974ad923255bb2fc31afbffeb60ce364e
Alina Buzachis <abuzachis@redhat.com>
2026-09-15 12:57:47 +0200

    Deprecate Amazon Inspector Classic service
    Signed-off-by: Alina Buzachis <abuzachis@redhat.com>

--- plugins/modules/inspector_target.py
+++ plugins/modules/inspector_target.py
@@ -16,6 +16,11 @@ description:
     The usage did not change.
 author:
   - "Dennis Conrad (@dennisconrad)"
+
+deprecated:
+  removed_in: 12.0.0
+  why: Amazon has retired the "Amazon Inspector Classic" service.
+
 options:
   name:
     description:
